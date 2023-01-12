15038f1a7fafc832eff938479d8ae80e90bfc918
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-01-11 21:48:33 -0300

    add aliases to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,31 @@
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@assets': path.resolve(__dirname, '../src/assets'),
+      '@components': path.resolve(__dirname, '../src/components'),
+      '@services': path.resolve(__dirname, '../src/services'),
+      '@slices': path.resolve(__dirname, '../src/slices'),
+      '@utils': path.resolve(__dirname, '../src/utils'),
+      '@views': path.resolve(__dirname, '../src/views'),
+      '@config': path.resolve(__dirname, '../src/config'),
+      '@store': path.resolve(__dirname, '../src/store'),
+      '@layouts': path.resolve(__dirname, '../src/layouts'),
+    }
+
+    return config
+  },
 }
