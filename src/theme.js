e24a2aa32aba3559db0dcb65d0d1574208d813e8
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-01-11 21:48:13 -0300

    updates on button component

--- src/theme.js
+++ src/theme.js
@@ -8,12 +8,30 @@ export default extendTheme({
         backgroundColor: 'primary.400',
         textTransform: 'uppercase',
         fontWeight: 'normal',
+        paddingTop: 6,
+        paddingBottom: 6,
+        paddingLeft: 20,
+        paddingRight: 20,
         _hover: {
           bg: 'primary.300',
           backgroundColor: 'primary.300',
           _disabled: {
             bg: 'primary.400',
             backgroundColor: 'primary.400',
+          },
+        },
+      },
+      variants: {
+        secondary: {
+          color: 'white',
+          bg: 'transparent',
+          backgroundColor: 'transparent',
+          border: '1px solid',
+          borderColor: 'secondary.400',
+          _hover: {
+            bg: 'secondary.400',
+            backgroundColor: 'secondary.400',
+            color: 'primary.400',
           },
         },
       },
@@ -31,6 +49,10 @@ export default extendTheme({
     primary: {
       300: '#34218C',
       400: '#6847FF',
+    },
+
+    secondary: {
+      400: '#00E4C9',
     },
   },
 })
