f39afddffb7ec8a45c137df7668c448fcd0e4b3c
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-04-14 19:38:59 -0300

    change Notifications import to use aliases

--- src/components/header/Notifications.spec.jsx
+++ src/components/header/Notifications.spec.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 
-import Notifications from './Notifications'
+import { Notifications } from '@components'
 
 describe('Notifications Component', () => {
   it('should renders correctly', () => {
