4d8b56a9a31b033b04070e18c69670e5bd068cf2
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-04-13 22:22:09 -0300

    create tests for Header component

---
+++ src/components/header/Header.spec.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+import Header from '@components/header/Header'
+import UserReducer from '@slices/UserSlice'
+import { configureStore } from '@reduxjs/toolkit'
+
+describe('Header Component', () => {
+  const user = {
+    account: {
+      username: 'fakeUser',
+      avatar: {
+        small: 'fakeImg'
+      }
+    }
+  }
+
+  it('should renders correctly', () => {
+    const store = configureStore({
+      reducer: UserReducer,
+      preloadedState: { user },
+    });
+
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Header />
+        </Provider>
+      </BrowserRouter>
+    )
+
+    expect(screen.getByText('Jogar')).toBeInTheDocument()
+    expect(screen.getByText('Ola!')).toBeInTheDocument()
+  })
+
+  it('should renders with user', () => {
+    const store = configureStore({
+      reducer: UserReducer,
+      preloadedState: { user },
+    });
+
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Header />
+        </Provider>
+      </BrowserRouter>
+    )
+
+
+    expect(screen.getByText('fakeUser')).toBeInTheDocument()
+  })
+})
