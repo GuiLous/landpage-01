6626ce44fbb68d9629157bf84a1bd264b2736c9a
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-04-19 18:23:42 -0300

    create MatchStatsTable component

---
+++ src/components/match/MatchStatsTable.jsx
@@ -0,0 +1,126 @@
+import {
+  Avatar,
+  Box,
+  HStack,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react'
+import { useSelector } from 'react-redux'
+
+import { LevelBadge } from '@components'
+
+import style from './MatchStatsTable.module.css'
+
+export default function MatchStatsTable({ stats }) {
+  const user = useSelector((state) => state.user)
+
+  const isMyTeam = stats.players.find((player) => player.user_id === user.id)
+
+  const players = stats.players
+
+  return (
+    <TableContainer className={style.tableContainer}>
+      <Table variant="striped" bgColor="gray.600" colorScheme="stripe">
+        <Thead>
+          <Tr>
+            <Th className={isMyTeam ? style.myTeam : style.enemyTeam}>
+              {isMyTeam ? 'Meu Time' : 'Time Inimigo'}
+            </Th>
+            <Th>K</Th>
+            <Th>D</Th>
+            <Th>A</Th>
+            <Th>HS</Th>
+            <Th>HS%</Th>
+            <Th>P</Th>
+            <Th>DEF</Th>
+            <Th>FK</Th>
+            <Th>KDR</Th>
+            <Th>D/H</Th>
+            <Th>2k</Th>
+            <Th>3k</Th>
+            <Th>4k</Th>
+            <Th>5k</Th>
+            <Th>AFK</Th>
+            <Th textTransform="initial">1vsX</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {players.map((player) => {
+            //calculate head shots percent
+            const hsPercent = Number(
+              (player.stats.head_shots * 100) /
+                (player.stats.chest_shots +
+                  player.stats.other_shots +
+                  player.stats.head_shots)
+            ).toFixed(2)
+
+            //calculate Kill-Death Ratio number
+            const kdr = Number(
+              player.stats.kills / player.stats.deaths
+            ).toFixed(2)
+
+            //calculate Damage per Hit Ratio number
+            const dh = Number(
+              player.stats.damage /
+                (player.stats.chest_shots +
+                  player.stats.other_shots +
+                  player.stats.head_shots)
+            ).toFixed(2)
+
+            //calculate total clutchs
+            const oneVsX =
+              player.stats.clutch_v1 +
+              player.stats.clutch_v2 +
+              player.stats.clutch_v3 +
+              player.stats.clutch_v4 +
+              player.stats.clutch_v5
+
+            return (
+              <Tr>
+                <Td className={style.user}>
+                  <HStack>
+                    <Avatar
+                      variant="online"
+                      width="40px"
+                      height="40px"
+                      src={player.avatar?.small}
+                      borderWidth={2}
+                    />
+
+                    <Box className={style.level}>
+                      <LevelBadge level={player.level} xxsmall />
+                    </Box>
+
+                    <Text fontWeight={600}>{player.username}</Text>
+                  </HStack>
+                </Td>
+                <Td>{player.stats.kills}</Td>
+                <Td>{player.stats.deaths}</Td>
+                <Td>{player.stats.assists}</Td>
+                <Td>{player.stats.head_shots}</Td>
+                <Td>{hsPercent}%</Td>
+                <Td>{player.stats.plants}P</Td>
+                <Td>{player.stats.defuses}D</Td>
+                <Td>{player.stats.firstkills}</Td>
+                <Td>{kdr}</Td>
+                <Td>{dh}</Td>
+                <Td>{player.stats.double_kills}</Td>
+                <Td>{player.stats.triple_kills}</Td>
+                <Td>{player.stats.quadra_kills}</Td>
+                <Td>{player.stats.aces}</Td>
+                <Td>{player.stats.afk}</Td>
+                <Td>{oneVsX}</Td>
+              </Tr>
+            )
+          })}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  )
+}
