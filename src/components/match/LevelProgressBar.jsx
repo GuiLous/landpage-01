c09d0c7fad544596df1118199ab77adf9b1f2a44
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-04-18 23:29:54 -0300

    create logic to calcule progress when earnedPoints is negative

--- src/components/match/LevelProgressBar.jsx
+++ src/components/match/LevelProgressBar.jsx
@@ -8,13 +8,13 @@ import { Container, LevelBadge } from '@components'
 
 import style from './LevelProgressBar.module.css'
 
-export default function LevelProgressBar({ earnedPoints = -10 }) {
+export default function LevelProgressBar({ earnedPoints }) {
   const user = useSelector((state) => state.user)
 
-  const [userLevel, setUserLevel] = useState(0)
-  const [nextLevelValue, setNextLevelValue] = useState(1)
-  const [userPoints, setUserPoints] = useState(90)
-  const [totalEarnedPoints, setTotalEarnedPoints] = useState(0)
+  const [userLevel, setUserLevel] = useState(user.account.level)
+  const [nextLevelValue, setNextLevelValue] = useState(user.account.level + 1)
+  const [userPoints, setUserPoints] = useState(0)
+  const [pointsToRender, setPointsToRender] = useState(0)
   const [isNextLevel, setIsNextLevel] = useState(false)
   const [isPreviousLevel, setIsPreviousLevel] = useState(false)
 
@@ -39,28 +39,45 @@ export default function LevelProgressBar({ earnedPoints = -10 }) {
   }, [isPreviousLevel])
 
   useEffect(() => {
-    const pointsDiffTo100 = 100 - userPoints
-
     if (userPoints + earnedPoints >= 100) {
-      setTotalEarnedPoints(pointsDiffTo100)
+      const pointsDiffTo100 = 100 - userPoints
+
+      setPointsToRender(pointsDiffTo100)
 
       setIsPreviousLevel(false)
       setIsNextLevel(true)
 
       setTimeout(() => {
         setUserPoints(0)
-        setTotalEarnedPoints(0)
+        setPointsToRender(0)
 
-        if (earnedPoints + userPoints === 100) return
+        if (userPoints + earnedPoints === 100) return
 
         setTimeout(() => {
-          setTotalEarnedPoints((earnedPoints - pointsDiffTo100) % 100)
+          setPointsToRender((earnedPoints - pointsDiffTo100) % 100)
         }, 100)
       }, 1500)
-    } else if (false) {
-      console.log('aqui falso')
+    } else if (userPoints + earnedPoints < 0) {
+
+      setPointsToRender(userPoints * -1)
+
+      if (userLevel > 0) {
+        setIsPreviousLevel(true)
+        setIsNextLevel(false)
+
+        setTimeout(() => {
+          setUserPoints(100)
+          setPointsToRender(0)
+
+          if (userPoints + earnedPoints === -1) return
+
+          setTimeout(() => {
+            setPointsToRender(earnedPoints + userPoints)
+          }, 100)
+        }, 1500)
+      }
     } else {
-      setTotalEarnedPoints(earnedPoints)
+      setPointsToRender(earnedPoints)
     }
   }, [])
 
@@ -76,45 +93,45 @@ export default function LevelProgressBar({ earnedPoints = -10 }) {
         <LevelBadge level={userLevel} small />
       </Container>
 
-      <Container column gap={4} style={{ marginTop: '22px' }}>
+      <Container column gap={4} style={{ marginTop: '18px' }}>
         <Container
           className={style.progressBackGround}
-          style={{ position: earnedPoints < 0 ? 'relative' : 'initial' }}
+          style={{ position: pointsToRender < 0 ? 'relative' : 'initial' }}
         >
           <Container
             className={
-              earnedPoints < 0
+              pointsToRender < 0
                 ? `${style.initialProgress} ${style.transition}`
                 : style.initialProgress
             }
             style={{
               width:
-                Number(totalEarnedPoints) < 0
-                  ? `${userPoints + Number(totalEarnedPoints)}%`
+                pointsToRender < 0
+                  ? `${userPoints + pointsToRender}%`
                   : `${userPoints}%`,
-              position: earnedPoints < 0 ? 'absolute' : 'relative',
+              position: pointsToRender < 0 ? 'absolute' : 'relative',
             }}
           >
-            {Number(totalEarnedPoints) < 0 && (
-              <Text className={style.points} right={-12}>
+            {pointsToRender < 0 && (
+              <Text className={style.points} right={-16}>
                 <CountUp
-                  end={Number(totalEarnedPoints)}
-                  prefix={Number(totalEarnedPoints) >= 0 ? '+' : ''}
+                  end={pointsToRender}
+                  prefix={pointsToRender >= 0 ? '+' : ''}
                 />
                 pts
               </Text>
             )}
           </Container>
 
-          {Number(totalEarnedPoints) < 0 ? (
+          {pointsToRender < 0 ? (
             <Container
               className={style.progressDown}
               style={{ width: `${Math.abs(userPoints)}%` }}
-            ></Container>
+            />
           ) : (
             <Container
               style={{
-                maxWidth: `${Math.abs(totalEarnedPoints)}%`,
+                maxWidth: `${Math.abs(pointsToRender)}%`,
               }}
             >
               <Container className={`${style.progressBar} ${style.progressUp}`}>
@@ -126,23 +143,23 @@ export default function LevelProgressBar({ earnedPoints = -10 }) {
                   className={style.points}
                   right={
                     userLevel >= 30 // a imagem quando o level é maior ou igual a 30 ocupa mais espaço
-                      ? Number(totalEarnedPoints) <= 4
-                        ? Number(totalEarnedPoints) + userPoints >= 98
+                      ? pointsToRender <= 4
+                        ? pointsToRender + userPoints >= 98
                           ? '10px'
-                          : '-38px'
-                        : Number(totalEarnedPoints) + userPoints >= 98
-                        ? '10px'
+                          : pointsToRender === 0 ? '-54px' : '-38px'
+                        : pointsToRender + userPoints >= 98
+                          ? '10px'
+                          : '6px'
+                      : pointsToRender <= 3
+                        ? userPoints + pointsToRender >= 98
+                          ? '12px'
+                          : (pointsToRender === 0 ? '-46px' : '-30px')
                         : '6px'
-                      : Number(totalEarnedPoints) <= 3
-                      ? userPoints + Number(totalEarnedPoints) >= 98
-                        ? '6px'
-                        : '-30px'
-                      : '6px'
                   }
                 >
                   <CountUp
-                    end={Number(totalEarnedPoints)}
-                    prefix={Number(totalEarnedPoints) >= 0 ? '+' : ''}
+                    end={pointsToRender}
+                    prefix={pointsToRender >= 0 ? '+' : ''}
                   />
                   pts
                 </Text>
@@ -152,9 +169,14 @@ export default function LevelProgressBar({ earnedPoints = -10 }) {
         </Container>
 
         <Container style={{ padding: '0 10px' }} justify="between">
-          <Text fontSize={14}>CLASSIFICAÇÃO RANQUEADA</Text>
+          <Text fontSize={14}>
+            CLASSIFICAÇÃO RANQUEADA
+          </Text>
           <Text fontSize={12}>
-            <Text as="span" color="secondary.400">
+            <Text
+              as="span"
+              color="secondary.400"
+            >
               {userPoints}
             </Text>
 
