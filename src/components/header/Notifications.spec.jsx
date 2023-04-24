0e179aeb494dad4fbdd89a5541fd4015e3100926
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-04-24 16:36:29 -0300

    fix Notifications test

--- src/components/header/Notifications.spec.jsx
+++ src/components/header/Notifications.spec.jsx
@@ -1,16 +1,83 @@
+import { configureStore } from '@reduxjs/toolkit'
 import { render, screen } from '@testing-library/react'
 
 import { Notifications } from '@components'
+import UserReducer from '@slices/UserSlice'
+import { Provider } from 'react-redux'
 
 describe('Notifications Component', () => {
   it('should renders correctly', () => {
-    render(<Notifications totalNotifications={1} />)
+    const user = {
+      id: 1,
+      account: {
+        notifications: [
+          {
+            id: 1,
+            to_user_id: 2,
+            content: 'Nova atualização do FiveM disponível.',
+            avatar: 'https://github.com/GuiLous.png',
+            create_date: '2023-04-08T18:23:12',
+            from_user_id: null,
+            read_date: null,
+          },
+          {
+            id: 2,
+            to_user_id: 2,
+            content: 'Uma nova solicitação de amizade para você.',
+            avatar: 'https://github.com/GuiLous.png',
+            create_date: '2023-04-07T18:23:12',
+            from_user_id: 4,
+            read_date: '2023-04-07T18:23:12',
+          },
+          {
+            id: 3,
+            to_user_id: 2,
+            content: 'Uma nova solicitação de amizade para você.',
+            avatar: 'https://github.com/GuiLous.png',
+            create_date: '2023-04-07T18:23:12',
+            from_user_id: 5,
+            read_date: null,
+          },
+        ],
+      },
+    }
 
-    expect(screen.getByText('1')).toBeInTheDocument()
+    const store = configureStore({
+      reducer: {
+        user: UserReducer,
+      },
+      preloadedState: { user },
+    })
+
+    render(
+      <Provider store={store}>
+        <Notifications totalUnreadNotifications={2} />
+      </Provider>
+    )
+
+    expect(screen.getByText('2')).toBeInTheDocument()
   })
 
   it('should not renders badge notification if totalNotifications props equal 0', () => {
-    render(<Notifications totalNotifications={0} />)
+    const user = {
+      id: 1,
+      account: {
+        notifications: [],
+      },
+    }
+
+    const store = configureStore({
+      reducer: {
+        user: UserReducer,
+      },
+      preloadedState: { user },
+    })
+
+    render(
+      <Provider store={store}>
+        <Notifications totalUnreadNotifications={0} />
+      </Provider>
+    )
 
     expect(screen.queryByText('0')).not.toBeInTheDocument()
   })
