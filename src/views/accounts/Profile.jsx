752b21062b2d92bfe82596cb136cd3f660a8ee98
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-04-24 23:01:21 -0300

    add image on background and buttons to change page

--- src/views/accounts/Profile.jsx
+++ src/views/accounts/Profile.jsx
@@ -1,14 +1,54 @@
 import { useSelector } from 'react-redux'
 
-import { HeaderProfile } from '@components'
+import { Button, Flex } from '@chakra-ui/react'
 import { ProfileLayout } from '@layouts'
+
+import { HeaderProfile } from '@components'
+
+import { useState } from 'react'
+import style from './Profile.module.css'
 
 export default function ProfileView() {
   const user = useSelector((state) => state.user)
 
+  const [selectedButton, setSelectedButton] = useState('history')
+
   return (
     <ProfileLayout>
-      <HeaderProfile />
+      <Flex className={style.backgroundImg} />
+      <Flex direction="column" margin="0 auto">
+        <HeaderProfile />
+
+        <Flex align="center" justifyContent="flex-start" gap={4} mt={10}>
+          <Button
+            w="fit-content"
+            variant="secondary"
+            fontSize={12}
+            fontWeight={600}
+            color={selectedButton === 'history' ? 'secondary.400' : 'gray.700'}
+            borderColor={
+              selectedButton === 'history' ? 'secondary.400' : 'gray.700'
+            }
+            onClick={() => setSelectedButton('history')}
+          >
+            HISTÓRICO DE PARTIDAS
+          </Button>
+
+          <Button
+            w="fit-content"
+            variant="secondary"
+            fontSize={12}
+            fontWeight={600}
+            color={selectedButton === 'config' ? 'secondary.400' : 'gray.700'}
+            borderColor={
+              selectedButton === 'config' ? 'secondary.400' : 'gray.700'
+            }
+            onClick={() => setSelectedButton('config')}
+          >
+            CONFIGURAÇÕES
+          </Button>
+        </Flex>
+      </Flex>
     </ProfileLayout>
   )
 }
