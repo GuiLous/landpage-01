a7c91fe988d425f8adb6510be53ce33f46f76e40
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-02 15:52:28 -0300

    fix NotificationsMenuList story

--- src/components/header/NotificationsMenuList.stories.jsx
+++ src/components/header/NotificationsMenuList.stories.jsx
@@ -3,54 +3,49 @@ import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
 import { BellIcon, NotificationsMenuList } from '@components'
-import UserReducer from '@slices/UserSlice'
+import NotificationReducer from '@slices/NotificationSlice'
 
 export default {
   title: 'Header/NotificationsMenuList',
   component: NotificationsMenuList,
-  tags: ['autodocs'],
 }
 
-const user = {
-  id: 1,
-  account: {
-    notifications: [
-      {
-        id: 1,
-        to_user_id: 2,
-        content: 'Nova atualização do FiveM disponível.',
-        avatar: 'https://github.com/GuiLous.png',
-        create_date: '2023-04-08T18:23:12',
-        from_user_id: null,
-        read_date: null,
-      },
-      {
-        id: 2,
-        to_user_id: 2,
-        content: 'Pré-cadastro liberado.',
-        avatar: 'https://github.com/GuiLous.png',
-        create_date: '2023-04-08T18:23:12',
-        from_user_id: null,
-        read_date: '2023-04-08T18:23:12',
-      },
-      {
-        id: 3,
-        to_user_id: 2,
-        content: 'Lançamento dia DD/MM/YYYY.',
-        avatar: 'https://github.com/GuiLous.png',
-        create_date: '2023-04-08T18:23:12',
-        from_user_id: null,
-        read_date: null,
-      },
-    ],
+const notifications = [
+  {
+    id: 1,
+    to_user_id: 2,
+    content: 'Nova atualização do FiveM disponível.',
+    avatar: 'https://github.com/GuiLous.png',
+    create_date: '2023-04-08T18:23:12',
+    from_user_id: null,
+    read_date: '2023-04-07T18:23:12',
   },
-}
+  {
+    id: 2,
+    to_user_id: 2,
+    content:
+      'Uma nova solicitação de amizade para você. Verifique suas notificações.',
+    avatar: 'https://github.com/GuiLous.png',
+    create_date: '2023-04-07T18:23:12',
+    from_user_id: 4,
+    read_date: null,
+  },
+  {
+    id: 3,
+    to_user_id: 2,
+    content: 'Uma nova solicitação de amizade para você.',
+    avatar: 'https://github.com/GuiLous.png',
+    create_date: '2023-04-07T18:23:12',
+    from_user_id: 5,
+    read_date: '2023-04-07T18:23:12',
+  },
+]
 
 const store = configureStore({
   reducer: {
-    user: UserReducer,
+    notifications: NotificationReducer,
   },
-  preloadedState: { user },
+  preloadedState: { notifications },
 })
 
 export const Default = {
