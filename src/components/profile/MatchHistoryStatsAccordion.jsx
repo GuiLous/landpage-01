661cfe07b028cf2b427f07230d93ead052d711f2
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-02 17:37:30 -0300

    change html structure and sizes

--- src/components/profile/MatchHistoryStatsAccordion.jsx
+++ src/components/profile/MatchHistoryStatsAccordion.jsx
@@ -5,7 +5,6 @@ import {
   AccordionItem,
   AccordionPanel,
   Avatar,
-  Box,
   Text,
 } from '@chakra-ui/react'
 
@@ -13,7 +12,19 @@ import { Container } from '@components'
 
 import style from './MatchHistoryStatsAccordion.module.css'
 
-export default function MatchHistoryStatsAccordion({ win = false }) {
+export default function MatchHistoryStatsAccordion() {
+  const won = false
+
+  const user = {
+    status: 'online',
+    account: {
+      avatar: {
+        medium:
+          'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
+      },
+    },
+  }
+
   return (
     <Accordion
       allowMultiple
@@ -22,110 +33,154 @@ export default function MatchHistoryStatsAccordion({ win = false }) {
       borderRadius={8}
       bgColor="gray.900"
     >
-      <AccordionItem pl={3} pr={2} py={3} border="none">
-        <Container align="center">
-          <Box
-            borderRadius={8}
-            h="49px"
-            as="div"
-            w="2px"
-            bgColor={win ? 'success' : 'danger.400'}
+      <AccordionItem px="16px" py="18px" border="none">
+        <Container align="stretch">
+          <Container
+            className={[style.borderLeft, won && style.won].join(' ')}
           />
 
-          <Container className={style.containerInfos}>
-            <Container className={style.avatarInfos} gap={8}>
-              <Avatar variant="online" size={'md'} src={null} borderWidth={2} />
+          <Container className={style.leftInfo} gap="10px" align="center">
+            <Avatar
+              variant={user.status}
+              size="lg"
+              src={user.account.avatar.medium}
+            />
 
-              <Container column gap={3}>
-                <Text
-                  as="span"
-                  fontSize={14}
-                  fontWeight={500}
-                  lineHeight="1"
-                  letterSpacing="0.5pt"
-                  color={win ? 'success' : 'danger.400'}
-                >
-                  {win ? 'Vitória' : 'Derrota'}
-                </Text>
-                <Text
-                  lineHeight="1"
-                  color="gray.700"
-                  fontSize={10}
-                  fontWeight={500}
-                >
-                  san andreas
-                </Text>
-              </Container>
-            </Container>
-
-            <Container className={style.resultInfo}>
-              <Container>
-                <Text
-                  as="span"
-                  fontSize={18}
-                  fontWeight={700}
-                  textAlign="center"
-                  color="#fff"
-                >
-                  10
-                  <Text
-                    pos="relative"
-                    top={-1}
-                    color="gray.700"
-                    fontSize={14}
-                    w="14px"
-                    as="span"
-                    display="inline-block"
-                  >
-                    :
-                  </Text>
-                  10
-                </Text>
-              </Container>
-
-              <Container column>
-                <Text as="span" fontSize={12} fontWeight={700} color="#fff">
-                  17/17/17
-                </Text>
-                <Text as="span" color="gray.700" fontSize={10}>
-                  1.35 KDA
-                </Text>
-              </Container>
-
-              <Container column>
-                <Text as="span" fontSize={12} fontWeight={700} color="#fff">
-                  0.40 KPR
-                </Text>
-                <Text as="span" color="gray.700" fontSize={10}>
-                  72.30 ADR
-                </Text>
-              </Container>
-
-              <Container column>
-                <Text as="span" fontSize={12} fontWeight={700} color="#fff">
-                  7.3% HS
-                </Text>
-                <Text as="span" color="gray.700" fontSize={10}>
-                  117 Pontuação média
-                </Text>
-              </Container>
-            </Container>
-
-            <Container className={style.seeMore}>
-              <Text as="span" fontSize={10} color="gray.700" mr={1}>
-                Abril 8 - 02:24
+            <Container column gap="8px">
+              <Text
+                as="span"
+                fontSize={20}
+                fontWeight={500}
+                lineHeight="1"
+                letterSpacing="0.5pt"
+                color={won ? 'success' : 'danger.400'}
+              >
+                {won ? 'Vitória' : 'Derrota'}
               </Text>
-              <AccordionButton width="fit-content" p={0} color="secondary.400">
-                <Text as="span" textAlign="left" fontSize={12}>
-                  Ver detalhes
-                </Text>
-                <AccordionIcon />
-              </AccordionButton>
+              <Text
+                lineHeight="1"
+                color="gray.700"
+                fontSize={14}
+                fontWeight={500}
+              >
+                san andreas
+              </Text>
             </Container>
+          </Container>
+
+          <Container className={style.centerInfo} align="center" gap="32px">
+            <Container className={style.statsCenterInfos}>
+              <Text
+                as="span"
+                fontSize={24}
+                fontWeight={700}
+                textAlign="center"
+                color="#fff"
+              >
+                10
+                <Text
+                  pos="relative"
+                  top={-1}
+                  color="gray.700"
+                  fontSize={20}
+                  w="14px"
+                  as="span"
+                  display="inline-block"
+                >
+                  :
+                </Text>
+                10
+              </Text>
+            </Container>
+
+            <Container column className={style.statsCenterInfos}>
+              <Text
+                as="span"
+                fontSize={16}
+                fontWeight={700}
+                color="#fff"
+                align="flex-start"
+              >
+                17/17/17
+              </Text>
+              <Text
+                as="span"
+                color="gray.700"
+                fontWeight={500}
+                fontSize={14}
+                align="flex-start"
+              >
+                1.35 KDA
+              </Text>
+            </Container>
+
+            <Container column className={style.statsCenterInfos}>
+              <Text
+                as="span"
+                fontSize={16}
+                fontWeight={700}
+                color="#fff"
+                align="flex-start"
+              >
+                0.40 KPR
+              </Text>
+              <Text
+                as="span"
+                color="gray.700"
+                fontWeight={500}
+                fontSize={14}
+                align="flex-start"
+              >
+                72.30 ADR
+              </Text>
+            </Container>
+
+            <Container
+              column
+              style={{ minWidth: '100px' }}
+              align="flex-start"
+              className={style.statsCenterInfos}
+            >
+              <Text
+                as="span"
+                textAlign="left"
+                fontSize={16}
+                fontWeight={700}
+                color="#fff"
+              >
+                7.3% HS
+              </Text>
+              <Text
+                as="span"
+                color="gray.700"
+                fontWeight={500}
+                fontSize={14}
+                align="flex-start"
+              >
+                117 Pontuação média
+              </Text>
+            </Container>
+          </Container>
+
+          <Container
+            className={style.rightInfo}
+            column
+            align="center"
+            justify="center"
+          >
+            <Text as="span" fontSize={14} fontWeight={500} color="gray.700">
+              Abril 8 - 02:24
+            </Text>
+            <AccordionButton width="fit-content" p={0} color="secondary.400">
+              <Text as="span" textAlign="left" fontSize={14}>
+                Ver detalhes
+              </Text>
+              <AccordionIcon />
+            </AccordionButton>
           </Container>
         </Container>
 
-        <AccordionPanel pb={4}>oadslf</AccordionPanel>
+        <AccordionPanel>content</AccordionPanel>
       </AccordionItem>
     </Accordion>
   )
