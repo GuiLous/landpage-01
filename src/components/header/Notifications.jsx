6f24fae2680daed8d15ec69d200eb7121f20dbd3
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-02 15:07:28 -0300

    remove totalUnreadNotifications prop and get notifications from slice

--- src/components/header/Notifications.jsx
+++ src/components/header/Notifications.jsx
@@ -1,11 +1,18 @@
 import { Badge, Box, Icon, Menu, MenuButton } from '@chakra-ui/react'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 import { BellIcon, NotificationsMenuList } from '@components'
 
 import style from './Notifications.module.css'
 
-export default function Notifications({ totalUnreadNotifications }) {
+export default function Notifications() {
+  const userNotifications = useSelector((state) => state.notifications)
+
+  const totalUnreadNotifications = userNotifications.filter(
+    (notification) => notification.read_date === null
+  ).length
+
   return (
     <Box className={style.notificationBox}>
       <Menu placement="bottom-end">
