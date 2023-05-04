8111a05ff25af91d9c6b9dfa0f8ed0c00d61332f
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-04 18:40:19 -0300

    create LevelCardStats stories

---
+++ src/components/profile/LevelCardStats.stories.jsx
@@ -0,0 +1,10 @@
+import LevelCardStats from './LevelCardStats'
+
+export default {
+  title: 'Profile/LevelCardStats',
+  component: LevelCardStats,
+}
+
+export const Default = {
+  render: (props) => <LevelCardStats {...props} />,
+}
