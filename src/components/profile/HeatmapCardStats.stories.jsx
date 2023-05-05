7117f459d7df82e9643e5bc79415fc9c4842e4e2
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-05 16:19:57 -0300

    create HeatmapCardStats stories

--- src/components/profile/HeatmapCardStats.stories.jsx
+++ src/components/profile/HeatmapCardStats.stories.jsx
@@ -3,6 +3,18 @@ import HeatmapCardStats from './HeatmapCardStats'
 export default {
   title: 'Profile/HeatmapCardStats',
   component: HeatmapCardStats,
+  argTypes: {
+    profile: { control: 'object' },
+  },
+  args: {
+    profile: {
+      stats: {
+        head_shots: 10,
+        chest_shots: 30,
+        other_shots: 60,
+      },
+    },
+  },
 }
 
 export const Default = {
