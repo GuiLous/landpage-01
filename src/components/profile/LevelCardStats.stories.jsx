6c4e18de25ce9201564718fbbb50734d8dbea9b2
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-04 21:49:35 -0300

    pass args to stories

--- src/components/profile/LevelCardStats.stories.jsx
+++ src/components/profile/LevelCardStats.stories.jsx
@@ -3,6 +3,23 @@ import LevelCardStats from './LevelCardStats'
 export default {
   title: 'Profile/LevelCardStats',
   component: LevelCardStats,
+  argTypes: {
+    user: { control: 'object' },
+    match: { control: 'object' },
+  },
+  args: {
+    user: {
+      level: 50,
+      wins: 10,
+      loses: 5,
+      win_rate: 68,
+      kda_rate: 0.88,
+      adr_rate: 93.5,
+      hs_percent: 8,
+      combat_points: 173,
+      economic_points: 8,
+    },
+  },
 }
 
 export const Default = {
