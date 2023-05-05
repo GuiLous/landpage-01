6ae0015bdc40cf1ec04039b02cb4ea8f91423635
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-05 13:23:47 -0300

    create HeatmapCardStats stories

---
+++ src/components/profile/HeatmapCardStats.stories.jsx
@@ -0,0 +1,10 @@
+import HeatmapCardStats from './HeatmapCardStats'
+
+export default {
+  title: 'Profile/HeatmapCardStats',
+  component: HeatmapCardStats,
+}
+
+export const Default = {
+  render: (props) => <HeatmapCardStats {...props} />,
+}
