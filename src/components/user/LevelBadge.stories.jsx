38bceb407104d09fe45bdc6c43ff397bfc815234
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-05-11 20:35:30 -0300

    add fitParent prop on LevelBadge

--- src/components/user/LevelBadge.stories.jsx
+++ src/components/user/LevelBadge.stories.jsx
@@ -1,18 +1,26 @@
-import LevelBadge from './LevelBadge'
+import { Container, LevelBadge } from '@components'
 
 export default {
   title: 'User/LevelBadge',
   component: LevelBadge,
   argTypes: {
     level: { control: { type: 'range', min: 0, max: 50 } },
-    size: { control: 'select', options: ['xs', 'sm', 'md', 'lg', 'xl'] },
+    size: { control: 'select', options: ['xs', 'sm', 'md', 'lg', 'xl', ''] },
+    fitParent: { control: 'boolean' },
+    fontSize: { control: 'number' },
   },
   args: {
     level: 0,
     size: 'md',
+    fitParent: false,
+    fontSize: null,
   },
 }
 
 export const Default = {
-  render: (props) => <LevelBadge {...props} />,
+  render: (props) => (
+    <Container>
+      <LevelBadge {...props} />
+    </Container>
+  ),
 }
