0cd93ba88c0eb6df714703b2c89b4426a7368298
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-05-12 16:35:25 -0300

    fix tests

--- src/components/profile/LevelCardStats.spec.jsx
+++ src/components/profile/LevelCardStats.spec.jsx
@@ -3,29 +3,39 @@ import { render, screen } from '@testing-library/react'
 import { LevelCardStats } from '@components'
 
 describe('LevelCardStats Component', () => {
-  const profile = {
-    level: 50,
+  const props = {
+    level: 20,
+    highest_level: 23,
+    matches_played: 340,
+    match_wins: 102,
+    highest_win_streak: 8,
+    latest_matches_results: ['V', 'D', 'D', 'V', 'V'],
     stats: {
-      wins: 10,
-      loses: 5,
-      krd_rate: 0.88,
-      economic_points: 8,
-      adr_rate: 93.5,
-      max_kills: 15,
-      hs_percent: 88,
-      max_damage: 190,
-      combat_points: 400,
+      kills: 240,
+      deaths: 640,
+      assists: 350,
+      damage: 65020,
+      hs_kills: 45,
+      clutch_v1: 39,
+      clutch_v2: 25,
+      clutch_v3: 9,
+      clutch_v4: 0,
+      clutch_v5: 1,
+      shots_fired: 4500,
+      head_shots: 45,
+      chest_shots: 4065,
+      other_shots: 390,
+      most_kills_in_a_match: 14,
+      most_damage_in_a_match: 890,
     },
-    last_five_matches: ['win', 'loss', 'win', 'loss', 'loss'],
   }
 
   it('should renders correctly', () => {
-    render(<LevelCardStats profile={profile} />)
+    render(<LevelCardStats {...props} />)
 
     expect(screen.getByText('VITÓRIAS')).toBeInTheDocument()
-    expect(screen.getByText('ABATES/MORTES')).toBeInTheDocument()
-    expect(screen.getByText('DANO/RODADA')).toBeInTheDocument()
-    expect(screen.getByText('88%')).toBeInTheDocument()
-    expect(screen.getByText('0.88')).toBeInTheDocument()
+    expect(screen.getByText('KDR')).toBeInTheDocument()
+    expect(screen.getByText('MAIOR LEVEL')).toBeInTheDocument()
+    expect(screen.getByText('1%')).toBeInTheDocument()
   })
 })
