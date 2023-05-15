d70da6a8b5a9cd616508e8ea076704197a7f99a7
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-05-15 00:09:55 -0300

    fix missing css module for Avatar component

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,12 +1,11 @@
-import { Avatar as ChakraAvatar, AvatarBadge } from '@chakra-ui/react'
+import { AvatarBadge, Avatar as ChakraAvatar } from '@chakra-ui/react'
 import React from 'react'
 
 import { Container } from '@components'
-import style from './Avatar.module.css'
 
 export default function Avatar(props) {
   return (
-    <Container className={style.container}>
+    <Container>
       <ChakraAvatar {...props}>
         <AvatarBadge />
       </ChakraAvatar>
