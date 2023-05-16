f576cd2894924db7e7769d136f3014de08d560b0
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-05-16 05:10:13 -0300

    add font weights to theme

--- src/theme.js
+++ src/theme.js
@@ -523,4 +523,15 @@ export default extendTheme({
       100: '#111111',
     },
   },
+  fontWeights: {
+    thin: 100,
+    extraLight: 200,
+    light: 300,
+    regular: 400,
+    medium: 500,
+    semiBold: 600,
+    bold: 700,
+    extraBold: 800,
+    black: 900,
+  },
 })
