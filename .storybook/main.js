7ff87fac4a76dad2ed6d791ed16cf85fbcf8229d
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-05-16 05:15:04 -0300

    add hooks folder and useOutsideClick hook

--- .storybook/main.js
+++ .storybook/main.js
@@ -32,6 +32,7 @@ const config = {
       '@config': path.resolve(__dirname, '../src/config'),
       '@store': path.resolve(__dirname, '../src/store'),
       '@layouts': path.resolve(__dirname, '../src/layouts'),
+      '@hooks': path.resolve(__dirname, '../src/hooks'),
     }
 
     return config
