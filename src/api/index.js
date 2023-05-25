08bdb499097f3ccecf6195c3c571c0cf62a1acf0
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-25 14:57:48 -0300

    create ProfilesAPI interface

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,12 @@ import AccountsAPI from './Accounts'
 import FriendsAPI from './Friends'
 import MatchmakingAPI from './Matchmaking'
 import NotificationsAPI from './Notifications'
+import ProfilesAPI from './Profiles'
 
-export { NotificationsAPI, AccountsAPI, MatchmakingAPI, FriendsAPI }
+export {
+  NotificationsAPI,
+  AccountsAPI,
+  MatchmakingAPI,
+  FriendsAPI,
+  ProfilesAPI,
+}
