fa6241fbc7249c9b929b5c851b18a3aa2d9c770b
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-25 14:58:05 -0300

    remove footer

--- src/layouts/ProfileLayout.jsx
+++ src/layouts/ProfileLayout.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux'
 
-import { Container, Footer, Header } from '@components'
+import { Container, Header } from '@components'
 
 import style from './ProfileLayout.module.css'
 
@@ -12,8 +12,6 @@ export default function ProfileLayout(props) {
       {user && <Header />}
 
       <Container className={style.content}>{props.children}</Container>
-
-      <Footer />
     </Container>
   )
 }
