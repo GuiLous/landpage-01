8dd2e20f030fae4c2aaf71dde2f8837da798662d
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-24 23:08:25 -0300

    add detail method

--- src/api/Accounts.js
+++ src/api/Accounts.js
@@ -10,6 +10,10 @@ export const AccountsAPI = {
   async listMatches(token, user_id) {
     return await BaseAPI.list(`accounts/${user_id}/matches/`, token)
   },
+
+  async detail(token, user_id) {
+    return await BaseAPI.detail(`accounts/profiles/${user_id}/`, token)
+  },
 }
 
 export default AccountsAPI
