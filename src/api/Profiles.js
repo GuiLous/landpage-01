15841e44923b84f8a05670dc475eb5c3be5364ee
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-29 15:12:15 -0300

    add / on url

--- src/api/Profiles.js
+++ src/api/Profiles.js
@@ -2,7 +2,7 @@ import { BaseAPI } from './Base'
 
 export const ProfilesAPI = {
   async detail(token, user_id) {
-    return await BaseAPI.detail(`profiles?user_id=${user_id}`, token)
+    return await BaseAPI.detail(`profiles/?user_id=${user_id}`, token)
   },
 }
 
