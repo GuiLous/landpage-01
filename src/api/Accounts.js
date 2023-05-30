2005098ed31792504ff5318905c4e5a1c12469f2
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-29 23:10:34 -0300

    add update method

--- src/api/Accounts.js
+++ src/api/Accounts.js
@@ -6,6 +6,10 @@ export const AccountsAPI = {
     // https://github.com/3C-gg/reload-backend/issues/401
     return await BaseAPI.update('accounts/logout/', token)
   },
+
+  async update(token, email) {
+    return await BaseAPI.update('accounts/update-email/', token, email)
+  },
 }
 
 export default AccountsAPI
