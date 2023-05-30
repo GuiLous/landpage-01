8522b970216d0967667f3831c0ed133f21fca091
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-30 16:47:46 -0300

    fix stories

--- src/components/account/InactivateAccountCard.stories.jsx
+++ src/components/account/InactivateAccountCard.stories.jsx
@@ -1,29 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
 
 import { InactivateAccountCard } from '@components'
-import UserReducer from '@slices/UserSlice'
+import { BrowserRouter } from 'react-router-dom'
 
 export default {
   title: 'Account/InactivateAccountCard',
   component: InactivateAccountCard,
 }
 
-const user = {
-  id: 1,
-}
-
-const store = configureStore({
-  reducer: {
-    user: UserReducer,
-  },
-  preloadedState: { user },
-})
+const mockStore = configureStore()({})
 
 export const Default = {
   render: (props) => (
-    <Provider store={store}>
-      <InactivateAccountCard {...props} />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={mockStore}>
+        <InactivateAccountCard {...props} />
+      </Provider>
+    </BrowserRouter>
   ),
 }
