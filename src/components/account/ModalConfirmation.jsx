0c6278ffb7c6297780f29f6686c5f9851426e7aa
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-30 13:52:20 -0300

    create ModalConfirmation component

---
+++ src/components/account/ModalConfirmation.jsx
@@ -0,0 +1,56 @@
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from '@chakra-ui/react'
+
+import { Container } from '@components'
+
+export default function ModalConfirmation({
+  isOpen,
+  onClose,
+  title,
+  children,
+}) {
+  return (
+    <Modal
+      size="3xl"
+      isCentered
+      isOpen={isOpen}
+      onClose={onClose}
+      scrollBehavior="inside"
+    >
+      <ModalOverlay />
+      <ModalContent py="40px">
+        <ModalHeader p={0} mb="40px">
+          <Container justify="center">
+            {title && (
+              <Text
+                color="white"
+                fontSize={20}
+                textTransform="uppercase"
+                lineHeight={1}
+                data-testid="title"
+              >
+                {title}
+              </Text>
+            )}
+          </Container>
+          <ModalCloseButton
+            fontSize={15}
+            width="fit-content"
+            height="fit-content"
+            top={5}
+            right={5}
+          />
+        </ModalHeader>
+
+        <ModalBody p={0}>{children}</ModalBody>
+      </ModalContent>
+    </Modal>
+  )
+}
