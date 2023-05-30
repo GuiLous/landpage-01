41187260daa02504e025f509ce2435a784bc0595
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-30 18:46:11 -0300

    add more tests for InactivateAccountCard component

--- src/components/account/InactiveAccountCard.spec.jsx
+++ src/components/account/InactiveAccountCard.spec.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import configureStore from 'redux-mock-store'
@@ -24,5 +24,55 @@ describe('InactivateAccountCard Component', () => {
       )
     ).toBeInTheDocument()
     expect(screen.getByText('Prosseguir com a inativação')).toBeInTheDocument()
+  })
+
+  it('should open the modal when button is clicked', () => {
+    render(
+      <Provider store={mockStore}>
+        <BrowserRouter>
+          <InactivateAccountCard />
+        </BrowserRouter>
+      </Provider>
+    )
+
+    expect(
+      screen.queryByText(
+        'Aviso! Depois que sua conta for suspensa, você terá que aguardar 30 dias para reativá-la novamente ou entrar em contato com o suporte para revogá-la.'
+      )
+    ).not.toBeInTheDocument()
+
+    fireEvent.click(screen.getByText('Prosseguir com a inativação'))
+
+    expect(
+      screen.getByText(
+        'Aviso! Depois que sua conta for suspensa, você terá que aguardar 30 dias para reativá-la novamente ou entrar em contato com o suporte para revogá-la.'
+      )
+    ).toBeInTheDocument()
+  })
+
+  it('should close the modal when the close button is clicked', () => {
+    render(
+      <Provider store={mockStore}>
+        <BrowserRouter>
+          <InactivateAccountCard />
+        </BrowserRouter>
+      </Provider>
+    )
+
+    fireEvent.click(screen.getByText('Prosseguir com a inativação'))
+
+    expect(
+      screen.getByText(
+        'Aviso! Depois que sua conta for suspensa, você terá que aguardar 30 dias para reativá-la novamente ou entrar em contato com o suporte para revogá-la.'
+      )
+    ).toBeInTheDocument()
+
+    fireEvent.click(screen.getByLabelText('Close'))
+
+    expect(
+      screen.queryByText(
+        'Aviso! Depois que sua conta for suspensa, você terá que aguardar 30 dias para reativá-la novamente ou entrar em contato com o suporte para revogá-la.'
+      )
+    ).not.toBeInTheDocument()
   })
 })
