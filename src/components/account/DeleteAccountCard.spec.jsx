cd65eaf7d60a82f6f7f8e0202bf76c36a8312dbb
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-05-30 23:06:29 -0300

    create DeleteAccountCard tests

---
+++ src/components/account/DeleteAccountCard.spec.jsx
@@ -0,0 +1,63 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import configureStore from 'redux-mock-store'
+
+import { DeleteAccountCard } from '@components'
+
+describe('DeleteAccountCard Component', () => {
+  const mockStore = configureStore()({})
+
+  it('should render correctly', () => {
+    render(
+      <Provider store={mockStore}>
+        <BrowserRouter>
+          <DeleteAccountCard />
+        </BrowserRouter>
+      </Provider>
+    )
+
+    expect(screen.getByText('EXCLUIR CONTA')).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        'Exclua permanentemente a sua conta. Você perderá todo histórico de partidas e o seu elo atual.'
+      )
+    ).toBeInTheDocument()
+    expect(screen.getByText('Prosseguir com a exclusão')).toBeInTheDocument()
+  })
+
+  it('should open the modal when button is clicked', () => {
+    render(
+      <Provider store={mockStore}>
+        <BrowserRouter>
+          <DeleteAccountCard />
+        </BrowserRouter>
+      </Provider>
+    )
+
+    fireEvent.click(screen.getByText('Prosseguir com a exclusão'))
+
+    const modal = screen.getByRole('dialog')
+    expect(modal).toBeInTheDocument()
+  })
+
+  it('should close the modal when the close button is clicked', () => {
+    render(
+      <Provider store={mockStore}>
+        <BrowserRouter>
+          <DeleteAccountCard />
+        </BrowserRouter>
+      </Provider>
+    )
+
+    fireEvent.click(screen.getByText('Prosseguir com a exclusão'))
+
+    const modal = screen.getByRole('dialog')
+
+    expect(modal).toBeInTheDocument()
+
+    fireEvent.click(screen.getByLabelText('Close'))
+
+    expect(modal).not.toBeInTheDocument()
+  })
+})
