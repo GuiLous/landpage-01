b2e66a812c56fefe32fb77d4c4a456d36c2177b5
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-06-01 16:30:42 -0300

    change path to /conta

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,7 @@ export default function App() {
         />
 
         <Route
-          path="/conta/:userId"
+          path="/conta"
           element={
             <RequireAuth
               user={user}
