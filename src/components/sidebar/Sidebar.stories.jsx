9eb7bef20f23ce024d3ad273f5cbaf0b43c902a9
Gabriel Gularte <gabrielfgularte@gmail.com>
2023-06-01 15:19:22 -0300

    add new styles and behaviour to sidebar to match new design

--- src/components/sidebar/Sidebar.stories.jsx
+++ src/components/sidebar/Sidebar.stories.jsx
@@ -3,171 +3,69 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import { Container, Sidebar } from '@components'
-import FriendReducer from '@slices/FriendSlice'
 import InviteReducer from '@slices/InviteSlice'
+import NotificationReducer from '@slices/NotificationSlice'
 import UserReducer from '@slices/UserSlice'
 
 export default {
   title: 'Sidebar/Sidebar',
   component: Sidebar,
-  argTypes: {},
-  args: {},
-  parameters: {
-    mockData: [
-      {
-        url: 'http://localhost:8000/api/friends/',
-        method: 'GET',
-        status: 200,
-        response: {
-          online: [
-            {
-              id: 2,
-              status: 'online',
-              username: 'Amigo 2',
-              avatar:
-                'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
-            },
-            {
-              id: 4,
-              status: 'online',
-              username: 'Amigo 4',
-              avatar:
-                'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
-            },
-            {
-              id: 5,
-              status: 'online',
-              username: 'Amigo 5',
-              avatar:
-                'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
-            },
-            {
-              id: 6,
-              status: 'online',
-              username: 'Amigo 6',
-              avatar:
-                'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
-            },
-            {
-              id: 7,
-              status: 'online',
-              username: 'Amigo 7',
-              avatar:
-                'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
-            },
-            {
-              id: 8,
-              status: 'online',
-              username: 'Amigo 8',
-              avatar:
-                'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
-            },
-            {
-              id: 9,
-              status: 'online',
-              username: 'Amigo 9',
-              avatar:
-                'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
-            },
-            {
-              id: 10,
-              status: 'online',
-              username: 'Amigo 10',
-              avatar:
-                'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
-            },
-            {
-              id: 11,
-              status: 'online',
-              username: 'Amigo 11',
-              avatar:
-                'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
-            },
-            {
-              id: 12,
-              status: 'online',
-              username: 'Amigo 12',
-              avatar:
-                'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
-            },
-            {
-              id: 13,
-              status: 'online',
-              username: 'Amigo 13',
-              avatar:
-                'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
-            },
-          ],
-          offline: [
-            {
-              id: 3,
-              status: 'offline',
-              username: 'Amigo 3',
-              avatar:
-                'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
-            },
-          ],
-        },
-      },
-      {
-        url: 'http://localhost:8000/api/mm/lobby/1/invite-player/2/',
-        method: 'POST',
-        status: 200,
-        response: {
-          to_player: { id: 2 },
-        },
-      },
-    ],
+  argTypes: {
+    unreadInvites: { control: { type: 'range', min: 0 } },
+    unreadNotifications: { control: { type: 'range', min: 0 } },
+    collapsed: { control: 'boolean' },
+    userLevel: { control: { type: 'range', min: 0, max: 50 } },
+  },
+  args: {
+    unreadInvites: 0,
+    unreadNotifications: 0,
+    collapsed: true,
+    userLevel: 0,
   },
 }
-
-const user = {
-  id: 1,
-  account: {
-    lobby: {
-      id: 1,
-      players_ids: [1],
-      max_players: 5,
-      player_count: 1,
-    },
-    avatar: {
-      medium:
-        'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
-    },
-    lobby_invites: [],
-    lobby_invites_sent: [],
-    username: 'Username',
-  },
-}
-
-const friends = {
-  online: [],
-  offline: [],
-}
-
-const invites = {
-  received: [],
-  sent: [],
-  unread: 0,
-}
-
-const store = configureStore({
-  reducer: {
-    user: UserReducer,
-    friends: FriendReducer,
-    invites: InviteReducer,
-  },
-  preloadedState: { user, friends, invites },
-})
 
 export const Default = {
-  render: (props) => (
-    <BrowserRouter>
-      <Provider store={store}>
-        <Container style={{ height: '90vh' }} column>
-          <Sidebar {...props} />
-        </Container>
-      </Provider>
-    </BrowserRouter>
-  ),
+  render: (props) => {
+    const user = {
+      id: 1,
+      account: {
+        level: props.userLevel,
+        level_points: 56,
+        avatar: {
+          medium:
+            'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg',
+        },
+        username: 'Username',
+      },
+    }
+
+    const invites = {
+      received: [],
+      sent: [],
+      unread: props.unreadInvites,
+    }
+
+    const notifications = Array.from(
+      Array(props.unreadNotifications).keys()
+    ).map((friend, index) => ({ id: index }))
+
+    const store = configureStore({
+      reducer: {
+        user: UserReducer,
+        notifications: NotificationReducer,
+        invites: InviteReducer,
+      },
+      preloadedState: { user, notifications, invites },
+    })
+
+    return (
+      <BrowserRouter>
+        <Provider store={store}>
+          <Container style={{ height: '100vh' }} column>
+            <Sidebar {...props} />
+          </Container>
+        </Provider>
+      </BrowserRouter>
+    )
+  },
 }
