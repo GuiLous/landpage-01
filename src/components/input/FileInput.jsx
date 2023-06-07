af7d474ec26e6c2842079a541a21901bef480015
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-06-07 18:22:07 -0300

    create FileInput component

---
+++ src/components/input/FileInput.jsx
@@ -0,0 +1,82 @@
+import { Box, Image, Input, Text } from '@chakra-ui/react';
+import { useState } from 'react';
+
+import uploadImg from '@assets/images/upload.png';
+
+import { Container } from '@components';
+
+import style from './FileInput.module.css';
+
+export default function FileInput({ setFile }) {
+
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+    const droppedFile = e.dataTransfer.files[0];
+    setFile(droppedFile);
+  };
+
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+  };
+
+  return (
+    <Box
+      className={[style.container, isDragging && style.dragging].join(' ')}
+      onDrop={handleDrop}
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}
+      onDragOver={(e) => e.preventDefault()}
+    >
+      <Input
+        type="file"
+        opacity={0}
+        position="absolute"
+        top={0}
+        h="full"
+        cursor="pointer"
+        label=""
+      />
+
+      <Container className={style.content} column gap={16}>
+        <Container fitContent align="center" justify="center">
+          <Image src={uploadImg} alt="upload image" w="36px" h="24px" />
+        </Container>
+
+        <Container fitContent gap={14} column align="center" justify="center">
+          <Text fontSize={12} fontWeight="regular" lineHeight={1} color="gray.700" as="span">
+            Solte os arquivos aqui ou{' '}
+            <Text fontWeight="semiBold" color="primary.400" as="span">
+              Procure
+            </Text>
+          </Text>
+
+          <Container column gap={8} align="center" justify="center">
+            <Text fontSize={10} lineHeight={1} fontWeight="regular" color="gray.700" as="span">
+              Arquivos suportados:{' '}
+              <Text fontWeight="medium" as="span">
+                JPG, PNG, GIF, PDF
+              </Text>
+            </Text>
+
+            <Text fontSize={10} lineHeight={1} fontWeight="regular" color="gray.700" as="span">
+              Tamanho máximo:{' '}
+              <Text fontWeight="medium" as="span">
+                3MB
+              </Text>
+            </Text>
+          </Container>
+
+        </Container>
+      </Container>
+    </Box>
+  )
+}
