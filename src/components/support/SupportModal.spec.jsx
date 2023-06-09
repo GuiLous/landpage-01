9d8ed5c2705c60a4e6ba177962f828211c2802ae
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-06-09 18:20:46 -0300

    fi xSupportModal tests

--- src/components/support/SupportModal.spec.jsx
+++ src/components/support/SupportModal.spec.jsx
@@ -1,9 +1,18 @@
-import { SupportModal } from '@components'
 import { render, screen } from '@testing-library/react'
+import configureStore from 'redux-mock-store'
+
+import { SupportModal } from '@components'
+import { Provider } from 'react-redux'
 
 describe('SupportModal Component', () => {
+  const mockStore = configureStore()({})
+
   it('should render correctly', async () => {
-    render(<SupportModal isOpen={true} onClose={() => {}} />)
+    render(
+      <Provider store={mockStore}>
+        <SupportModal isOpen={true} onClose={() => {}} />
+      </Provider>
+    )
 
     expect(
       screen.getByText(
