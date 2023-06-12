984162fa2ab88c5e30bdb4267512e7627bbe9161
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-06-12 07:34:04 -0300

    add listTickets method

--- src/api/Support.js
+++ src/api/Support.js
@@ -1,8 +1,12 @@
 import { BaseAPI } from './Base'
 
 export const SupportAPI = {
+  async listTickets(token) {
+    return await BaseAPI.list('support/tickets/subjects/', token)
+  },
+
   async createTicket(token, form) {
-    return await BaseAPI.create('support/ticket/', token, form)
+    return await BaseAPI.create('support/tickets/', token, form)
   },
 }
 
