65828bf261a5d40fad9224455c0be7381f3ca7c1
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-06-14 07:53:11 -0300

    fix tests

--- src/components/sidebar/Sidebar.spec.jsx
+++ src/components/sidebar/Sidebar.spec.jsx
@@ -80,7 +80,7 @@ const server = setupServer(
   })
 )
 
-beforeAll(() => server.listen())
+beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }))
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
@@ -100,12 +100,8 @@ describe('Sidebar Component', () => {
         id: 1,
       },
     },
+    lobby_id: 1,
     status: 'online',
-  }
-
-  const lobby = {
-    queue: null,
-    id: 1,
   }
 
   const invites = {
