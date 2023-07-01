14b62282e83e5a9191a1f72e3838e148f2776bf8
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-06-30 22:31:49 -0300

    create Maintenance slice

---
+++ src/slices/MaintenanceSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const MaintenanceReducer = createSlice({
+  name: 'maintenance',
+  initialState: true,
+  reducers: {
+    updateMaintenance: (state, action) => {
+      state = action.payload
+      return state
+    },
+  },
+})
+
+export const { updateMaintenance } = MaintenanceReducer.actions
+
+export default MaintenanceReducer.reducer
--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ import AppReducer from './slices/AppSlice'
 import FriendReducer from './slices/FriendSlice.js'
 import InviteReducer from './slices/InviteSlice'
 import LobbyReducer from './slices/LobbySlice'
+import MaintenanceReducer from './slices/MaintenanceSlice'
 import MatchReducer from './slices/MatchSlice'
 import NotificationReducer from './slices/NotificationSlice'
 import PreMatchReducer from './slices/PreMatchSlice'
@@ -19,6 +20,7 @@ export default configureStore({
     app: AppReducer,
     lobby: LobbyReducer,
     preMatch: PreMatchReducer,
+    maintenance: MaintenanceReducer,
   },
   devTools: true,
 })
