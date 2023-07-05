6d8e06ed61f0936ee2d469b070ef89fc0b5a5f25
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-07-05 08:32:07 -0300

    remove MaintenanceSlice

--- src/store.js
+++ src/store.js
@@ -4,7 +4,6 @@ import AppReducer from './slices/AppSlice'
 import FriendReducer from './slices/FriendSlice.js'
 import InviteReducer from './slices/InviteSlice'
 import LobbyReducer from './slices/LobbySlice'
-import MaintenanceReducer from './slices/MaintenanceSlice'
 import MatchReducer from './slices/MatchSlice'
 import NotificationReducer from './slices/NotificationSlice'
 import PreMatchReducer from './slices/PreMatchSlice'
@@ -20,7 +19,6 @@ export default configureStore({
     app: AppReducer,
     lobby: LobbyReducer,
     preMatch: PreMatchReducer,
-    maintenance: MaintenanceReducer,
   },
   devTools: true,
 })
