5ddd166367e9312e6b790aad62195c1bb29863ef
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-07-06 21:38:04 -0300

    update LobbyView to make design more responsive

--- src/views/lobby/LobbyView.jsx
+++ src/views/lobby/LobbyView.jsx
@@ -1,4 +1,4 @@
-import { Badge, Icon, Text } from '@chakra-ui/react'
+import { Badge, Icon, Text, useMediaQuery } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
@@ -17,6 +17,8 @@ import { removeRestartQueue } from '@slices/LobbySlice'
 import style from './LobbyView.module.css'
 
 export default function LobbyView() {
+  const [isLessThan2xl] = useMediaQuery('(max-width: 1600px)')
+
   const user = useSelector((state) => state.user)
   const lobby = useSelector((state) => state.lobby)
   const preMatch = useSelector((state) => state.preMatch)
@@ -82,14 +84,31 @@ export default function LobbyView() {
   }, [preMatch])
 
   return (
-    <Container className={style.container} column gap={40} align="center">
+    <Container
+      className={style.container}
+      column
+      gap={isLessThan2xl ? 30 : 40}
+      align="center"
+    >
       <Container className={style.header} gap={12} fitContent>
-        <Icon as={JoystickIcon} color="white" fontSize={30} />
+        <Icon
+          as={JoystickIcon}
+          color="white"
+          fontSize={{ base: 30, md: 28, '2xl': 30 }}
+        />
         <Container gap={8}>
-          <Text fontSize={22} fontWeight="light" textTransform="uppercase">
+          <Text
+            fontSize={{ base: 20, md: 18, '2xl': 20 }}
+            fontWeight="light"
+            textTransform="uppercase"
+          >
             Suba de nível e
           </Text>
-          <Text fontSize={22} fontWeight="semibold" textTransform="uppercase">
+          <Text
+            fontSize={{ base: 20, md: 18, '2xl': 20 }}
+            fontWeight="semibold"
+            textTransform="uppercase"
+          >
             fique entre os melhores
           </Text>
         </Container>
@@ -106,8 +125,10 @@ export default function LobbyView() {
           gap={12}
           align="center"
         >
-          <Text fontSize={18}>TDM 5x5</Text>
-          <Badge>Em breve</Badge>
+          <Text fontSize={{ base: 18, md: 16, '2xl': 18 }}>TDM 5x5</Text>
+          <Badge paddingBottom={{ base: '4px', md: '3px', '2xl': '4px' }}>
+            Em breve
+          </Badge>
         </Container>
 
         <Container
@@ -119,7 +140,7 @@ export default function LobbyView() {
           <Container className={style.arrowUpIcon} justify="center">
             <Icon as={ArrowUpFilledIcon} color="primary.400" />
           </Container>
-          <Text fontSize={18}>Ranqueada 5x5</Text>
+          <Text fontSize={{ base: 18, md: 16, '2xl': 18 }}>Ranqueada 5x5</Text>
         </Container>
 
         <Container
@@ -128,8 +149,10 @@ export default function LobbyView() {
           gap={12}
           align="center"
         >
-          <Text fontSize={18}>Personalizada</Text>
-          <Badge>Em breve</Badge>
+          <Text fontSize={{ base: 18, md: 16, '2xl': 18 }}>Personalizada</Text>
+          <Badge paddingBottom={{ base: '4px', md: '3px', '2xl': '4px' }}>
+            Em breve
+          </Badge>
         </Container>
       </Container>
 
