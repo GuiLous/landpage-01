70fecebc7a198e3e2b07d403f77e812764d35f42
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-07-10 17:31:47 -0300

    add tooltip and link

--- src/components/sidebar/SidebarAvatarLink.jsx
+++ src/components/sidebar/SidebarAvatarLink.jsx
@@ -1,28 +1,39 @@
-import { Avatar, Text } from '@chakra-ui/react'
+import { Avatar, Link, Text, Tooltip } from '@chakra-ui/react'
+import { Link as RouterLink } from 'react-router-dom'
 
 import { Container } from '@components'
 
 export default function SidebarAvatarLink({ user }) {
   return (
-    <Container gap={14} align="center" justify="center">
-      <Avatar src={user.account.avatar.medium} variant={user.status} />
-      <Container column>
-        <Text
-          color="white"
-          fontWeight={'medium'}
-          fontSize={{ base: 16, md: 14, '2xl': 16 }}
-        >
-          {user.account.username}
-        </Text>
+    <Tooltip label="Meu perfil" aria-label="profile" placement="top-start" >
+      <Link
+        as={RouterLink}
+        to={`/perfil/${user.id}`}
+        gap="14px"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        w="full"
+      >
+        <Avatar src={user.account.avatar.medium} variant={user.status} />
+        <Container column>
+          <Text
+            color="white"
+            fontWeight={'medium'}
+            fontSize={{ base: 16, md: 14, '2xl': 16 }}
+          >
+            {user.account.username}
+          </Text>
 
-        <Text
-          color="gray.700"
-          fontWeight={'medium'}
-          fontSize={{ base: 12, md: 10, '2xl': 12 }}
-        >
-          LEVEL {user.account.level}
-        </Text>
-      </Container>
-    </Container>
+          <Text
+            color="gray.700"
+            fontWeight={'medium'}
+            fontSize={{ base: 12, md: 10, '2xl': 12 }}
+          >
+            LEVEL {user.account.level}
+          </Text>
+        </Container>
+      </Link>
+    </Tooltip>
   )
 }
