800e806a58801597e771b93361e92b00b6178284
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-07-17 22:55:13 -0300

    create LoadingTexts tests

---
+++ src/components/loading/LoadingTexts.spec.jsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+
+import { LoadingTexts } from '@components'
+
+describe('LoadingTexts Component', () => {
+  const textsArray = [
+    `Ligando as luzes`,
+  ]
+  const intervalTime = 3000
+
+  it('should render correctly', async () => {
+    render(
+      <LoadingTexts textsArray={textsArray} intervalTime={intervalTime} />
+    )
+
+    expect(screen.getByText('Ligando as luzes')).toBeInTheDocument()
+  })
+})
