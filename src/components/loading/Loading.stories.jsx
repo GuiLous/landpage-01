2aff1fa4e1fe6579614558cb70e4320825bc70cf
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-07-17 23:35:24 -0300

    create Loading stories

---
+++ src/components/loading/Loading.stories.jsx
@@ -0,0 +1,10 @@
+import { Loading } from '@components'
+
+export default {
+  title: 'Loading/Loading',
+  component: Loading,
+}
+
+export const Default = {
+  render: (props) => <Loading {...props} />,
+}
