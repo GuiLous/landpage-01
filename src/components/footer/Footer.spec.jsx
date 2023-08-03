6676528141aede429aba29f6ce5d3be4d5c03bc9
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-08-03 16:51:35 -0300

    improve and create Footer tests

--- src/components/footer/Footer.spec.jsx
+++ src/components/footer/Footer.spec.jsx
@@ -3,13 +3,17 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { Footer } from '@components'
 
-describe('NotFound Component', () => {
-  it('should render correctly', () => {
-    render(
-      <BrowserRouter>
-        <Footer />
-      </BrowserRouter>
-    )
+const renderComponent = () => {
+  return (
+    <BrowserRouter>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+describe('Footer Component', () => {
+  it('should render desktop correctly', () => {
+    render(renderComponent())
 
     expect(
       screen.getAllByText('Copyright ©2023. Todos os direitos reservados.')
@@ -17,5 +21,67 @@ describe('NotFound Component', () => {
     expect(
       screen.getAllByText('Copyright ©2023. Todos os direitos reservados.')[0]
     ).toBeInTheDocument()
+
+    expect(screen.getByTestId('logo')).toBeInTheDocument()
+
+    expect(screen.getByTestId('instagram')).toHaveAttribute(
+      'href',
+      'https://www.instagram.com/reloadclubgg/'
+    )
+
+    expect(screen.getByTestId('twitter')).toHaveAttribute(
+      'href',
+      'https://twitter.com/reloadclubgg'
+    )
+
+    expect(screen.getByTestId('discord')).toHaveAttribute(
+      'href',
+      'https://discord.gg/mMMKshktfT'
+    )
+
+    expect(screen.getByTestId('youtube')).toHaveAttribute(
+      'href',
+      'https://www.youtube.com/channel/UC0Yx6OapSWC0pym9ACd-D1A'
+    )
+
+    expect(screen.getByTestId('facebook')).toHaveAttribute(
+      'href',
+      'https://www.facebook.com/profile.php?id=100089787770305'
+    )
+  })
+
+  it('should render mobile correctly', () => {
+    render(renderComponent())
+
+    window.innerWidth = 320
+    window.dispatchEvent(new Event('resize'))
+
+    const logoMobileElement = screen.getByTestId('logoMobile')
+    expect(logoMobileElement).toBeInTheDocument()
+
+    expect(screen.getByTestId('instagramMobile')).toHaveAttribute(
+      'href',
+      'https://www.instagram.com/reloadclubgg/'
+    )
+
+    expect(screen.getByTestId('twitterMobile')).toHaveAttribute(
+      'href',
+      'https://twitter.com/reloadclubgg'
+    )
+
+    expect(screen.getByTestId('discordMobile')).toHaveAttribute(
+      'href',
+      'https://discord.gg/mMMKshktfT'
+    )
+
+    expect(screen.getByTestId('youtubeMobile')).toHaveAttribute(
+      'href',
+      'https://www.youtube.com/channel/UC0Yx6OapSWC0pym9ACd-D1A'
+    )
+
+    expect(screen.getByTestId('facebookMobile')).toHaveAttribute(
+      'href',
+      'https://www.facebook.com/profile.php?id=100089787770305'
+    )
   })
 })
