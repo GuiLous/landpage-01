dd68bdfb50b1d7b4d289401b406abfa3054173a1
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-08-09 18:32:57 -0300

    fix Match stories

--- src/views/match/Match.stories.jsx
+++ src/views/match/Match.stories.jsx
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
+import InviteReducer from '@slices/InviteSlice'
+import LobbyReducer from '@slices/LobbySlice'
 import MatchReducer from '@slices/MatchSlice'
 import UserReducer from '@slices/UserSlice'
 import { MatchView } from '@views'
@@ -155,12 +157,21 @@ export const Default = {
       id: 1,
     }
 
+    const invites = [{ to_player: { user_id: null } }]
+
+    const lobby = {
+      queue: null,
+      invited_players_ids: [],
+    }
+
     const store = configureStore({
       reducer: {
         user: UserReducer,
         match: MatchReducer,
+        invites: InviteReducer,
+        lobby: LobbyReducer,
       },
-      preloadedState: { user, match },
+      preloadedState: { user, match, invites, lobby },
     })
 
     return (
