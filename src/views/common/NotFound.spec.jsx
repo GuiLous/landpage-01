143a258cf16cadd097caf3e2a089d088ad897c23
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-08-23 21:39:02 -0300

    improve NotFound View tests

--- src/views/common/NotFound.spec.jsx
+++ src/views/common/NotFound.spec.jsx
@@ -3,14 +3,21 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { NotFoundView } from '@views'
 
-describe('NotFound Component', () => {
-  it('should render correctly', () => {
-    render(
-      <BrowserRouter>
-        <NotFoundView />
-      </BrowserRouter>
-    )
+const renderComponent = () => {
+  render(
+    <BrowserRouter>
+      <NotFoundView />
+    </BrowserRouter>
+  )
+}
 
+describe('NotFound View', () => {
+  it('should render correctly', () => {
+    renderComponent()
+
+    expect(screen.getByAltText('Logo')).toBeInTheDocument()
+    expect(screen.getByAltText('404')).toBeInTheDocument()
+    expect(screen.getByAltText('Error page')).toBeInTheDocument()
     expect(screen.getByText('Voltar para o início')).toBeInTheDocument()
   })
 })
