97af0d4d5fea279227f05823cdeec8b865aa7236
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-08-25 16:17:25 -0300

    change Connect to make more responsive

--- src/views/match/Connect.jsx
+++ src/views/match/Connect.jsx
@@ -1,4 +1,11 @@
-import { Box, Icon, Image, Text, Tooltip } from '@chakra-ui/react'
+import {
+  Box,
+  Icon,
+  Image,
+  Text,
+  Tooltip,
+  useMediaQuery,
+} from '@chakra-ui/react'
 import React, { useCallback, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
@@ -36,6 +43,7 @@ const textsArray = [
 
 export default function Connect() {
   const match = useSelector((state) => state.match)
+  const [isLessThan2xl] = useMediaQuery('(max-width: 1600px)')
 
   const navigate = useNavigate()
 
@@ -105,13 +113,21 @@ export default function Connect() {
         </Container>
       </Container>
 
-      <Container column style={{ marginBottom: '110px' }}>
-        <Container column align="end" justify="center" gap={80}>
+      <Container
+        column
+        style={{ marginBottom: isLessThan2xl ? '90px' : '110px' }}
+      >
+        <Container
+          column
+          align="end"
+          justify="center"
+          gap={isLessThan2xl ? 60 : 80}
+        >
           <Container className={style.logo} justify="end">
             <img src={logo} alt="ReloadClub" data-testid="logo" />
           </Container>
 
-          <Container column align="end" gap={18}>
+          <Container column align="end" gap={isLessThan2xl ? 16 : 18}>
             <Text className={style.title}>É hora do jogo!</Text>
             <Text className={style.helper}>
               Para se conectar, abra o FiveM e insira o IP abaixo na lista{' '}
@@ -125,7 +141,7 @@ export default function Connect() {
               <Container
                 className={style.connectionBox}
                 align="center"
-                gap={32}
+                gap={isLessThan2xl ? 28 : 32}
                 onClick={handleClipboard}
                 testID="clipboard"
               >
@@ -156,11 +172,19 @@ export default function Connect() {
           </Container>
 
           <Container justify="end" gap={14}>
-            <Text fontSize={24} color="white" fontWeight="semibold">
+            <Text
+              fontSize={{ base: 24, md: 22, '2xl': 24 }}
+              color="white"
+              fontWeight="semibold"
+            >
               <Timer initialTime={timeLeft || 1} reverse />
             </Text>
 
-            <Image src={loadingGif} alt="loading" width="48px" />
+            <Image
+              src={loadingGif}
+              alt="loading"
+              width={{ base: '48px', md: '44px', '2xl': '48px' }}
+            />
           </Container>
         </Container>
       </Container>
