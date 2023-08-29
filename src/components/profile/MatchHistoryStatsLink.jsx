8fa38812b82d5be9adffa7f960b63d15440b359d
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-08-29 18:52:09 -0300

    change MatchHistoryStatsLink to match new design

--- src/components/profile/MatchHistoryStatsLink.jsx
+++ src/components/profile/MatchHistoryStatsLink.jsx
@@ -1,4 +1,11 @@
-import { Icon, Link, Text, Tooltip, useMediaQuery } from '@chakra-ui/react'
+import {
+  Icon,
+  Image,
+  Link,
+  Text,
+  Tooltip,
+  useMediaQuery,
+} from '@chakra-ui/react'
 import { DateTime } from 'luxon'
 import { Link as RouterLink } from 'react-router-dom'
 
@@ -39,8 +46,36 @@ const statsFields = [
   },
 ]
 
-export default function MatchHistoryStatsLink({ match }) {
+const TYPE_MAP = {
+  competitive: 'Ranqueada',
+  custom: 'Personalizada',
+}
+
+export default function MatchHistoryStatsLink({ match, isLink = true }) {
   const [isLessThan2xl] = useMediaQuery('(max-width: 1600px)')
+
+  const startDate = DateTime.fromISO(match.start_date)
+  const endDate = match.end_date && DateTime.fromISO(match.end_date)
+  const elapsedTime = Math.floor(
+    endDate
+      ? endDate.diff(startDate, 'seconds').seconds
+      : DateTime.now().diff(startDate, 'seconds').seconds
+  )
+
+  function formatSeconds(seconds) {
+    const hours = Math.floor(seconds / 3600)
+    const minutes = Math.floor((seconds % 3600) / 60)
+    const remainingSeconds = seconds % 60
+
+    if (hours === 0)
+      return `${minutes.toString().padStart(2, '0')}m${remainingSeconds
+        .toString()
+        .padStart(2, '0')}s`
+
+    return `${hours.toString().padStart(2, '0')}h${minutes
+      .toString()
+      .padStart(2, '0')}m${remainingSeconds.toString().padStart(2, '0')}s`
+  }
 
   const renderStats = () => {
     return statsFields.map((stats, index) => (
@@ -73,8 +108,9 @@ export default function MatchHistoryStatsLink({ match }) {
           lineHeight={1}
           data-testid={stats.field}
         >
-          {match.stats[stats.field]}
-          {stats.title.includes('%') && '%'}
+          {match.status === 'running' && '-'}
+          {match.status !== 'running' && match.stats[stats.field]}
+          {match.status !== 'running' && stats.title.includes('%') && '%'}
         </Text>
       </Container>
     ))
@@ -82,60 +118,106 @@ export default function MatchHistoryStatsLink({ match }) {
 
   return (
     <Link
-      as={RouterLink}
-      to={`/partidas/${match.id}`}
+      as={isLink ? RouterLink : 'div'}
+      to={isLink ? `/partidas/${match.id}` : null}
       align="center"
+      cursor={isLink ? 'pointer' : 'initial'}
       data-testid="link"
-      className={[style.container, match.won ? style.won : style.defeated].join(
-        ' '
-      )}
+      className={[
+        style.container,
+        match.status !== 'running' && (match.won ? style.won : style.defeated),
+      ].join(' ')}
     >
       <Container className={style.border} fitContent></Container>
 
-      <Container column gap={isLessThan2xl ? 6 : 8}>
-        <Text
-          as="span"
-          fontSize={{ base: 20, md: 18, '2xl': 20 }}
-          fontWeight="medium"
-          lineHeight={1}
-          letterSpacing="0.5pt"
-          color="white"
-        >
-          {match.map_name || 'Nome do mapa'}
-        </Text>
-        <Text
-          lineHeight={1}
-          color="gray.300"
-          fontSize={{ base: 14, md: 12, '2xl': 14 }}
-          fontWeight="medium"
-        >
-          {DateTime.fromISO(match.end_date).toRelative()}
-        </Text>
+      <Container style={{ alignItems: 'initial' }} gap={16}>
+        <Image
+          src={match.map_url}
+          objectFit="cover"
+          borderRadius="4px"
+          w="54px"
+          h="54px"
+          alt="map name"
+          className={style.mapImage}
+        />
+
+        <Container column justify="between" gap={8}>
+          <Text
+            as="span"
+            fontSize={16}
+            fontWeight="medium"
+            lineHeight={1}
+            color="white"
+          >
+            {match.map_name || 'Nome do mapa'}
+          </Text>
+
+          <Container column fitContent gap={5}>
+            <Text lineHeight={1} color="gray.300" fontSize={12}>
+              {TYPE_MAP[match.game_type]}
+            </Text>
+
+            <Text lineHeight={1} color="gray.300" fontSize={12}>
+              {match.status === 'running' &&
+                `${startDate.toFormat('dd/MM/yyyy')} - `}
+              {formatSeconds(elapsedTime)}
+            </Text>
+          </Container>
+        </Container>
       </Container>
 
       <Container>
-        <Text
-          as="span"
-          fontSize={{ base: 24, md: 22, '2xl': 24 }}
-          fontWeight="semiBold"
-          textAlign="center"
-          color="white"
-          data-testid="score"
+        <Container
+          gap={14}
+          column
+          align="center"
+          style={{ maxWidth: 'fit-content', minWidth: '70px' }}
         >
-          {match.score}
-        </Text>
+          <Text
+            as="span"
+            fontSize={12}
+            fontWeight="medium"
+            textAlign="center"
+            color={
+              match.status === 'running'
+                ? 'gray.300'
+                : match.won
+                ? 'green.600'
+                : 'red.500'
+            }
+            lineHeight={1}
+          >
+            {match.status === 'running' && 'EM PARTIDA'}
+            {match.status !== 'running' && match.won && 'VITÓRIA'}
+            {match.status !== 'running' && !match.won && 'DERROTA'}
+          </Text>
+
+          <Text
+            as="span"
+            fontSize={20}
+            fontWeight="semiBold"
+            textAlign="center"
+            color="white"
+            data-testid="score"
+            lineHeight={1}
+          >
+            {match.score}
+          </Text>
+        </Container>
       </Container>
 
       <Container align="center" justify="center" gap={isLessThan2xl ? 30 : 40}>
         {renderStats()}
 
-        <Icon
-          as={ArrowRightSimpleIcon}
-          fill="white"
-          h="12px"
-          w="7px"
-          data-testid="arrow"
-        />
+        {isLink && (
+          <Icon
+            as={ArrowRightSimpleIcon}
+            fill="white"
+            h="12px"
+            w="7px"
+            data-testid="arrow"
+          />
+        )}
       </Container>
     </Link>
   )
