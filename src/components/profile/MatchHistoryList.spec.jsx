047196006450582270f8227fded2c05c8807f172
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-09-11 16:19:46 -0300

    fix tests

--- src/components/profile/MatchHistoryList.spec.jsx
+++ src/components/profile/MatchHistoryList.spec.jsx
@@ -1,68 +1,66 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { render, screen, waitFor } from '@testing-library/react'
-import { rest } from 'msw'
-import { setupServer } from 'msw/lib/node'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
+import { MatchesAPI } from '@api'
 import { MatchHistoryList } from '@components'
 import UserReducer from '@slices/UserSlice'
 
-const server = setupServer(
-  rest.get('http://localhost:8000/api/matches', (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        results: [
-          {
-            stats: {
-              kda: '0/0/0',
-              kdr: 2,
-              head_accuracy: 20,
-              adr: 33.33,
-              firstkills: 4,
-            },
-            id: 0,
-            score: '10:2',
-            start_date: '2023-05-05T10:20:00',
-            end_date: '2023-05-05T10:30:00',
-            won: true,
-            map_name: 'Auditório',
-            status: 'running',
-            map_image:
-              'https://static.wikia.nocookie.net/gtawiki/images/e/e8/SisyphusTheater-GTAV-Thumbnail.png',
-            game_type: 'competitive',
-          },
-          {
-            stats: {
-              kda: '0/0/0',
-              kdr: 2,
-              head_accuracy: 20,
-              adr: 33.33,
-              firstkills: 4,
-            },
-            id: 1,
-            score: '2:10',
-            start_date: '2023-05-05T10:20:00',
-            end_date: '2023-05-05T10:30:00',
-            won: false,
-            map_name: 'Auditório',
-            status: 'finished',
-            map_image:
-              'https://static.wikia.nocookie.net/gtawiki/images/e/e8/SisyphusTheater-GTAV-Thumbnail.png',
-            game_type: 'competitive',
-          },
-        ],
-        count: 1,
-        page_size: 10,
-        total_pages: 1,
-        prev_page: null,
-        current_page: 1,
-        next_page: null,
-      })
-    )
-  })
-)
+jest.mock('@api', () => ({
+  MatchesAPI: {
+    list: jest.fn(),
+  },
+}))
+
+const fakeResponse = {
+  results: [
+    {
+      stats: {
+        kda: '0/0/0',
+        kdr: 2,
+        head_accuracy: 20,
+        adr: 33.33,
+        firstkills: 4,
+      },
+      id: 0,
+      score: '10:2',
+      start_date: '2023-05-05T10:20:00',
+      end_date: '2023-05-05T10:30:00',
+      won: true,
+      map_name: 'Auditório',
+      status: 'running',
+      map_image:
+        'https://static.wikia.nocookie.net/gtawiki/images/e/e8/SisyphusTheater-GTAV-Thumbnail.png',
+      game_type: 'competitive',
+    },
+    {
+      stats: {
+        kda: '0/0/0',
+        kdr: 2,
+        head_accuracy: 20,
+        adr: 33.33,
+        firstkills: 4,
+      },
+      id: 1,
+      score: '2:10',
+      start_date: '2023-05-05T10:20:00',
+      end_date: '2023-05-05T10:30:00',
+      won: false,
+      map_name: 'Auditório',
+      status: 'finished',
+      map_image:
+        'https://static.wikia.nocookie.net/gtawiki/images/e/e8/SisyphusTheater-GTAV-Thumbnail.png',
+      game_type: 'competitive',
+    },
+  ],
+  count: 1,
+  page_size: 10,
+  total_pages: 1,
+  prev_page: null,
+  current_page: 1,
+  next_page: null,
+}
 
 const renderComponent = (username = '', user_id = 1) => {
   const user = {
@@ -86,9 +84,7 @@ const renderComponent = (username = '', user_id = 1) => {
 }
 
 describe('MatchHistoryList Component', () => {
-  beforeAll(() => server.listen())
-  afterEach(() => server.resetHandlers())
-  afterAll(() => server.close())
+  beforeEach(() => MatchesAPI.list.mockResolvedValue(fakeResponse))
 
   it('should render correctly', async () => {
     renderComponent()
@@ -100,26 +96,41 @@ describe('MatchHistoryList Component', () => {
     expect(screen.getByText('Últimas Partidas')).toBeInTheDocument()
   })
 
-  it('should render message without username when there is no matches and user.id is equal to user_id', () => {
+  it('should render message without username when there is no matches and user.id is equal to user_id', async () => {
+    MatchesAPI.list.mockResolvedValue({
+      results: [],
+      count: 1,
+      page_size: 10,
+      total_pages: 0,
+      prev_page: null,
+      current_page: 1,
+      next_page: null,
+    })
+
     renderComponent()
 
-    expect(
-      screen.getByText('Ops, você ainda não tem partidas.')
-    ).toBeInTheDocument()
+    await screen.findByText('Ops, você ainda não tem partidas.')
   })
 
-  it('should render message with username when there is no matches and user.id is not equal to user_id', () => {
+  it('should render message with username when there is no matches and user.id is not equal to user_id', async () => {
+    MatchesAPI.list.mockResolvedValue({
+      results: [],
+      count: 1,
+      page_size: 10,
+      total_pages: 0,
+      prev_page: null,
+      current_page: 1,
+      next_page: null,
+    })
     renderComponent('User1', 2)
 
-    expect(
-      screen.getByText('Ops, User1 ainda não tem partidas.')
-    ).toBeInTheDocument()
+    await screen.findByText('Ops, User1 ainda não tem partidas.')
   })
 
   it('should render 0 Partidas if matches.length is 0', async () => {
     renderComponent()
 
-    screen.getAllByText('0 Partidas')
+    await screen.findAllByText('0 Partidas')
   })
 
   it('should render 2 Partidas if matches.length is 2', async () => {
@@ -129,42 +140,35 @@ describe('MatchHistoryList Component', () => {
   })
 
   it('should render 1 Partida if matches.length is 1', async () => {
-    server.use(
-      rest.get('http://localhost:8000/api/matches', (req, res, ctx) => {
-        return res(
-          ctx.status(200),
-          ctx.json({
-            results: [
-              {
-                stats: {
-                  kda: '0/0/0',
-                  kdr: 2,
-                  head_accuracy: 20,
-                  adr: 33.33,
-                  firstkills: 4,
-                },
-                id: 0,
-                score: '10:2',
-                start_date: '2023-05-05T10:20:00',
-                end_date: '2023-05-05T10:30:00',
-                won: true,
-                map_name: 'Auditório',
-                status: 'finished',
-                map_image:
-                  'https://static.wikia.nocookie.net/gtawiki/images/e/e8/SisyphusTheater-GTAV-Thumbnail.png',
-                game_type: 'competitive',
-              },
-            ],
-            count: 1,
-            page_size: 10,
-            total_pages: 0,
-            prev_page: null,
-            current_page: 1,
-            next_page: null,
-          })
-        )
-      })
-    )
+    MatchesAPI.list.mockResolvedValue({
+      results: [
+        {
+          stats: {
+            kda: '0/0/0',
+            kdr: 2,
+            head_accuracy: 20,
+            adr: 33.33,
+            firstkills: 4,
+          },
+          id: 0,
+          score: '10:2',
+          start_date: '2023-05-05T10:20:00',
+          end_date: '2023-05-05T10:30:00',
+          won: true,
+          map_name: 'Auditório',
+          status: 'finished',
+          map_image:
+            'https://static.wikia.nocookie.net/gtawiki/images/e/e8/SisyphusTheater-GTAV-Thumbnail.png',
+          game_type: 'competitive',
+        },
+      ],
+      count: 1,
+      page_size: 10,
+      total_pages: 0,
+      prev_page: null,
+      current_page: 1,
+      next_page: null,
+    })
 
     renderComponent()
 
@@ -180,42 +184,35 @@ describe('MatchHistoryList Component', () => {
   })
 
   it('should render pagination if totalPages > 1', async () => {
-    server.use(
-      rest.get('http://localhost:8000/api/matches', (req, res, ctx) => {
-        return res(
-          ctx.status(200),
-          ctx.json({
-            results: [
-              {
-                stats: {
-                  kda: '0/0/0',
-                  kdr: 2,
-                  head_accuracy: 20,
-                  adr: 33.33,
-                  firstkills: 4,
-                },
-                id: 0,
-                score: '10:2',
-                start_date: '2023-05-05T10:20:00',
-                end_date: '2023-05-05T10:30:00',
-                won: true,
-                map_name: 'Auditório',
-                status: 'finished',
-                map_image:
-                  'https://static.wikia.nocookie.net/gtawiki/images/e/e8/SisyphusTheater-GTAV-Thumbnail.png',
-                game_type: 'competitive',
-              },
-            ],
-            count: 1,
-            page_size: 10,
-            total_pages: 2,
-            prev_page: null,
-            current_page: 1,
-            next_page: null,
-          })
-        )
-      })
-    )
+    MatchesAPI.list.mockResolvedValue({
+      results: [
+        {
+          stats: {
+            kda: '0/0/0',
+            kdr: 2,
+            head_accuracy: 20,
+            adr: 33.33,
+            firstkills: 4,
+          },
+          id: 0,
+          score: '10:2',
+          start_date: '2023-05-05T10:20:00',
+          end_date: '2023-05-05T10:30:00',
+          won: true,
+          map_name: 'Auditório',
+          status: 'finished',
+          map_image:
+            'https://static.wikia.nocookie.net/gtawiki/images/e/e8/SisyphusTheater-GTAV-Thumbnail.png',
+          game_type: 'competitive',
+        },
+      ],
+      count: 1,
+      page_size: 10,
+      total_pages: 2,
+      prev_page: null,
+      current_page: 1,
+      next_page: null,
+    })
 
     renderComponent()
 
