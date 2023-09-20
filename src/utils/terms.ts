9f58667879df2416137f71fa48d45658c2d43c81
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-09-20 13:11:59 -0300

    add terms on utils

---
+++ src/utils/terms.ts
@@ -0,0 +1,7 @@
+const useTermsLink =
+  'https://reloadclub.freshdesk.com/support/solutions/articles/150000108190-termos-de-uso'
+
+const privacyPolicyLink =
+  'https://reloadclub.freshdesk.com/support/solutions/articles/150000108164-pol%C3%ADtica-de-privacidade'
+
+export { privacyPolicyLink, useTermsLink }
