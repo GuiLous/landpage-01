8f3313adf7cdecb8bf5c7e1fa8207573294a3401
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-09-20 12:14:01 -0300

    create LayoutHeader component

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,3 +1,4 @@
 /* eslint-disable prettier/prettier */
 export * from './home';
+export * from './signup';
 
---
+++ src/components/pages/signup/layoutHeader/LayoutHeader.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { IoIosArrowRoundBack } from 'react-icons/io'
+
+import { storageService } from '@/services'
+
+import { Link } from '@/components/shared'
+
+export function LayoutHeader() {
+  const handleCancel = () => {
+    storageService.remove('token')
+  }
+
+  return (
+    <header className="absolute left-8 top-8 max-w-fit flex-initial">
+      <Link
+        href="/"
+        className="flex items-center gap-2.5"
+        onClick={handleCancel}
+      >
+        <IoIosArrowRoundBack size={31} />
+        <span className="font-medium">Cancelar e sair</span>
+      </Link>
+    </header>
+  )
+}
