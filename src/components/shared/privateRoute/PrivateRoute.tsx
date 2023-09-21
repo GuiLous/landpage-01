6798ba57e5554ce8d2bf522b48a4411222578a1a
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-09-21 18:10:53 -0300

    redirect to not-found

--- src/components/shared/privateRoute/PrivateRoute.tsx
+++ src/components/shared/privateRoute/PrivateRoute.tsx
@@ -3,7 +3,7 @@
 import { usePathname, useRouter } from 'next/navigation'
 import { ReactNode, useCallback, useEffect, useState } from 'react'
 
-import { checkIsPublicRoute } from '@/functions'
+import { checkIfPathExists, checkIsPublicRoute } from '@/functions'
 
 import { httpService, storageService } from '@/services'
 
@@ -27,6 +27,7 @@ export function PrivateRoute({ children }: PrivateRouteProps) {
   const [isVerifying, setIsVerifying] = useState(true)
 
   const isPublicPage = checkIsPublicRoute(pathname)
+  const pathExists = checkIfPathExists(pathname)
 
   const verifyUserToRedirect = useCallback(() => {
     if (!user?.account?.is_verified && pathname === '/alterar-email') return
@@ -73,6 +74,12 @@ export function PrivateRoute({ children }: PrivateRouteProps) {
   )
 
   useEffect(() => {
+    if (pathname === '/not-found') {
+      return
+    }
+
+    if (!pathExists) return router.push('/not-found')
+
     const userToken = storageService.get('token')
 
     if (userToken && !user && pathname !== '/auth') {
@@ -92,6 +99,8 @@ export function PrivateRoute({ children }: PrivateRouteProps) {
     pathname,
     initializeUserSlice,
     user,
+    router,
+    pathExists,
   ])
 
   return (
