bb13e13fe2458d939726364e38f367c551f0d132
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-09-22 11:49:59 -0300

    create InactiveMessage component

---
+++ src/components/pages/inactive/inactiveMessage/InactiveMessage.tsx
@@ -0,0 +1,14 @@
+export function InactiveMessage() {
+  return (
+    <article className="mt-3 max-w-[500px] flex-initial flex-col items-center justify-center gap-8">
+      <p className="text-center text-sm">
+        Isso significa que ou você solicitou a inativação ou <br />
+        nossa equipe encontrou algum problema na sua conta.
+      </p>
+      <p className="text-center text-sm">
+        Se você quer reativar a sua conta e continuar jogando, <br />
+        entre em contato com nosso suporte em um dos nossos canais.
+      </p>
+    </article>
+  )
+}
