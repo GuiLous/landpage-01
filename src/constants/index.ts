9e1719ec110d7abf367e02081c02f161970a7a85
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-09-27 20:41:24 -0300

    add new constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
+// Routes
 export const APP_ROUTES = {
   private: {
     register: '/cadastrar',
@@ -14,6 +15,19 @@ export const APP_ROUTES = {
   },
 }
 
+// Pins input
 export const TOTAL_SIGNUP_PINS = 6
 
+// Maintenance
 export const MAINTENANCE_TIME_TO_CHECK_AGAIN = 1000 * 60 * 1 // 1 minute
+
+// Sidebar
+export const SIDEBAR_TOP_MENU_ITEMS = [
+  'amigos',
+  'notificações',
+  'ranking',
+  'loja',
+]
+export const SIDEBAR_BOTTOM_MENU_ITEMS = ['suporte', 'sair']
+export const SOONITEMS = ['ranking', 'loja']
+export const MENULINKS = ['loja', 'ranking']
