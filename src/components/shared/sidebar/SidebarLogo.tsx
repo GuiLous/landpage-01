c2a5f20115e4952259f4d0fbb4b8c236a6f4914a
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-09-27 20:44:56 -0300

    create SidebarLogo component

---
+++ src/components/shared/sidebar/SidebarLogo.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+import logoFull from '@/assets/images/logo_symbol_full.svg'
+
+import { Link } from '../link/Link'
+
+export function SidebarLogo() {
+  return (
+    <div className="flex-initial flex-col">
+      <div className="px-7 pb-0 pt-10 3xl:px-6 3xl:pt-8">
+        <Link href="/jogar">
+          <Image
+            priority
+            src={logoFull}
+            width={176}
+            data-testid="logo-full"
+            alt="Reload logo"
+          />
+        </Link>
+      </div>
+    </div>
+  )
+}
