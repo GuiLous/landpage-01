0de7b15592afea343520443db0f587eea6caf513
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-09-27 20:44:02 -0300

    create Timer component

---
+++ src/components/shared/timer/Timer.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import { formatSecondsToMinutes } from '@/functions'
+
+interface TimerProps {
+  initialTime: number
+  stop?: boolean
+  reverse?: boolean
+  formatted?: boolean
+}
+
+export function Timer({
+  initialTime,
+  stop = false,
+  reverse = false,
+  formatted = true,
+}: TimerProps) {
+  const [elapsed, setElapsed] = useState(initialTime)
+
+  useEffect(() => {
+    if (reverse && !initialTime) {
+      throw Object.assign(
+        new Error(
+          'An initial time is required for a reverse timer (countdown).'
+        ),
+        { code: 400 }
+      )
+    }
+
+    const interval = stop
+      ? undefined
+      : setInterval(() => {
+          if (reverse) {
+            if (elapsed > 0) setElapsed(elapsed - 1)
+            else clearInterval(interval)
+          } else setElapsed(elapsed + 1)
+        }, 1000)
+
+    return () => clearInterval(interval)
+  })
+
+  const render = () => {
+    if (!formatted) return elapsed
+
+    const timer = elapsed
+
+    return formatSecondsToMinutes(timer)
+  }
+
+  return render()
+}
