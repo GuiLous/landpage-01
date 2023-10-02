006379a5ff8ce3e7046a06c9a44c14368b61ed2e
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-10-02 13:20:34 -0300

    add error style

--- src/components/shared/select/SelectOptionSelectedWrapper.tsx
+++ src/components/shared/select/SelectOptionSelectedWrapper.tsx
@@ -8,11 +8,13 @@ import { useSelectContext } from '@/contexts/select'
 interface SelectOptionSelectedWrapperProps {
   className?: string
   children?: ReactNode
+  error?: boolean
 }
 
 export function SelectOptionSelectedWrapper({
   children,
   className,
+  error,
 }: SelectOptionSelectedWrapperProps) {
   const { isChecked, isFocused } = useSelectContext()
 
@@ -22,6 +24,7 @@ export function SelectOptionSelectedWrapper({
         'flex items-center justify-between rounded-[4px] border border-gray-700 bg-gray-1200 px-3 py-[0.57rem]',
         (isChecked || isFocused) &&
           'outline-none ring-1 ring-purple-400 hover:ring-purple-400',
+        error && 'ring-1 ring-red-500',
         className
       )}
     >
