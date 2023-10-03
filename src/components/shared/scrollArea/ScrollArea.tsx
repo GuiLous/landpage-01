64bddbe4d6f5da1fb1b7378a2cdd9dd827a58062
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-10-03 16:30:05 -0300

    create ScrollArea component

---
+++ src/components/shared/scrollArea/ScrollArea.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import * as PrimitiveScrollArea from '@radix-ui/react-scroll-area'
+import { ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface ScrollAreaProps {
+  children: ReactNode
+  rootStyle?: string
+}
+
+export function ScrollArea({ children, rootStyle }: ScrollAreaProps) {
+  return (
+    <PrimitiveScrollArea.Root
+      className={twMerge('max-h-screen overflow-hidden', rootStyle)}
+    >
+      <PrimitiveScrollArea.Viewport className="h-full w-full">
+        {children}
+      </PrimitiveScrollArea.Viewport>
+      <PrimitiveScrollArea.Scrollbar
+        className="flex min-w-[10px] touch-none select-none rounded-[4px] bg-black/10 p-0.5 transition-colors duration-[160ms] ease-out  data-[orientation=horizontal]:h-2.5 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col"
+        orientation="vertical"
+      >
+        <PrimitiveScrollArea.Thumb className="relative min-w-[10px] flex-1 rounded-[4px] bg-black/40 before:absolute before:left-1/2 before:top-1/2 before:h-full before:min-h-[44px] before:w-full before:min-w-[44px] before:-translate-x-1/2 before:-translate-y-1/2 before:content-['']" />
+      </PrimitiveScrollArea.Scrollbar>
+      <PrimitiveScrollArea.Scrollbar
+        className="flex min-w-[10px] touch-none select-none rounded-[4px] bg-black/10 p-0.5 transition-colors duration-[160ms] ease-out data-[orientation=horizontal]:h-2.5 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col"
+        orientation="horizontal"
+      >
+        <PrimitiveScrollArea.Thumb className="relative min-w-[10px] flex-1 rounded-[4px] bg-black/40 before:absolute before:left-1/2 before:top-1/2 before:h-full before:min-h-[44px] before:w-full before:min-w-[44px] before:-translate-x-1/2 before:-translate-y-1/2 before:content-['']" />
+      </PrimitiveScrollArea.Scrollbar>
+    </PrimitiveScrollArea.Root>
+  )
+}
