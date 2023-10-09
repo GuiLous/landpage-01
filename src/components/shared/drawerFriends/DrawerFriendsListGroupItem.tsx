a8d0c25789bf4c40422b7efad997792a55050290
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-10-09 15:33:56 -0300

    create DrawerFriendsListGroupItem component

---
+++ src/components/shared/drawerFriends/DrawerFriendsListGroupItem.tsx
@@ -0,0 +1,88 @@
+import { twMerge } from 'tailwind-merge'
+
+import { COLOR_STATUS, STATUS_MAP } from '@/constants'
+
+import { useAppSelector } from '@/store'
+import { Status } from '@/store/slices/userSlice'
+
+import { Avatar } from '@/components/shared'
+
+interface DrawerFriendsListGroupItemProps {
+  user_id: number
+  status: Status
+  avatar: string
+  username: string
+  lobby_id: number | null
+  steam_url: string
+}
+
+export function DrawerFriendsListGroupItem({
+  avatar,
+  lobby_id,
+  status,
+  steam_url,
+  user_id,
+  username,
+}: DrawerFriendsListGroupItemProps) {
+  const { user } = useAppSelector((state) => state.user)
+  const lobby = useAppSelector((state) => state.lobby)
+
+  const { invites } = useAppSelector((state) => state.invites)
+
+  let humanStatus = STATUS_MAP[status]
+
+  humanStatus += status === 'in_game' ? ' (RANKED 5X5)' : ''
+
+  const availableStatuses = ['online', 'away', 'teaming']
+  const alreadyInvitedByFriend = lobby?.invited_players_ids?.some(
+    (id) => id === user_id
+  )
+  const alreadyInvited =
+    invites.filter((invite) => invite.to_player.user_id === user_id).length >
+      0 || alreadyInvitedByFriend
+  const alreadyOnTeam = lobby?.players_ids?.includes(user_id)
+
+  const isAvailable =
+    !alreadyOnTeam && availableStatuses.includes(status) && !lobby?.queue
+
+  return (
+    <div className="group animate-fade cursor-pointer gap-4 py-1.5 pl-0 pr-5 hover:bg-[linear-gradient(90deg,_#33333380_0%,_#33333300_100%,_#1e1e1e)]">
+      <div className="min-h-full min-w-[4px] max-w-[4px] rounded-[0_4px_4px_0] bg-gray-400 opacity-0 group-hover:opacity-100" />
+
+      <div className="items-center gap-3.5 px-0 py-1">
+        <div
+          className={twMerge(
+            'flex-initial max-w-fit',
+            status === 'offline' && 'opacity-50'
+          )}
+        >
+          <Avatar
+            avatarUrl={avatar}
+            status={status}
+            alt="Perfil do usuário"
+            md
+          />
+        </div>
+
+        <div
+          className={twMerge(
+            'flex-col gap-0.5',
+            status === 'offline' && 'opacity-50'
+          )}
+        >
+          <span className="text-sm font-medium text-white 3xl:text-xs">
+            {username}
+          </span>
+          <span
+            className={twMerge(
+              'text-xs 3xl:text-[0.625rem]',
+              COLOR_STATUS[status]
+            )}
+          >
+            {alreadyOnTeam ? 'Em grupo' : humanStatus}
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
