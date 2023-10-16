3155ff441060cc6c02c54b9406ac24af650b0e5d
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-10-16 14:18:50 -0300

    create LineupHiddenBox component

---
+++ src/components/shared/lineup/LineupHiddenBox.stories.tsx
@@ -0,0 +1,10 @@
+import { LineupHiddenBox } from './LineupHiddenBox'
+
+export default {
+  title: 'Friends/LineupHiddenBox',
+  component: LineupHiddenBox,
+}
+
+export const Default = (props: any) => {
+  return <LineupHiddenBox {...props} />
+}
