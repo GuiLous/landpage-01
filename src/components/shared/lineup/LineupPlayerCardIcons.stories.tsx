8cdd6fad72cfc18b4de3f70aa77d35625a7e8892
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-10-16 14:17:22 -0300

    create LineupPlayerCardIcons component

---
+++ src/components/shared/lineup/LineupPlayerCardIcons.stories.tsx
@@ -0,0 +1,18 @@
+import { LineupPlayerCardIcons } from './LineupPlayerCardIcons'
+
+export default {
+  title: 'Friends/LineupPlayerCardIcons',
+  component: LineupPlayerCardIcons,
+  argTypes: {
+    steam_url: { control: 'text' },
+    matches_played: { control: 'number' },
+  },
+  args: {
+    steam_url: 'https://steamcommunity.com/profiles/76561198075990604',
+    user_id: 1,
+  },
+}
+
+export const Default = (props: any) => {
+  return <LineupPlayerCardIcons {...props} />
+}
