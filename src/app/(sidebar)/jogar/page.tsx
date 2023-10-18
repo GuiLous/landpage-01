d600d4ff85c48ae185826647606b3ecf1931b7cd
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-10-18 09:56:02 -0300

    change page name

--- src/app/(sidebar)/jogar/page.tsx
+++ src/app/(sidebar)/jogar/page.tsx
@@ -2,7 +2,7 @@ import { LobbyGameType, LobbyHeader } from '@/components/pages'
 
 import { Lineup } from '@/components/shared'
 
-export default function page() {
+export default function Play() {
   return (
     <main className="h-full flex-col items-center gap-10 bg-gray-1200 3xl:gap-8">
       <LobbyHeader />
