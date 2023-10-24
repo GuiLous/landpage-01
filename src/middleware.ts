8d2e90cad33006d650ca871ad0571f73b1550d69
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-10-24 10:51:21 -0300

    create middleware

---
+++ src/middleware.ts
@@ -0,0 +1,71 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { getPathToRedirect, isAuthPages } from '@/utils'
+
+import { verifyJwtToken } from '@/services'
+
+export type userAuthToken = {
+  id: number
+  email: string
+  username: string | null
+  is_active: boolean
+  is_verified: boolean
+  token: string
+}
+
+export async function middleware(req: NextRequest) {
+  const { nextUrl, cookies } = req
+
+  const { value: token } = cookies.get('token') ?? { value: null }
+
+  const hasVerifiedToken = token && (await verifyJwtToken(token))
+  const isAuthPageRequested = isAuthPages(nextUrl.pathname)
+
+  if (isAuthPageRequested) {
+    if (!hasVerifiedToken) {
+      const homeUrl = new URL('/', nextUrl.origin)
+
+      return NextResponse.redirect(homeUrl)
+    }
+
+    const redirectUrl = getPathToRedirect({
+      token,
+      urlOrigin: nextUrl.origin,
+      isAuthPath: true,
+      pathname: nextUrl.pathname,
+    })
+
+    if (redirectUrl !== undefined) {
+      return NextResponse.redirect(redirectUrl)
+    }
+
+    return NextResponse.next()
+  }
+
+  if (hasVerifiedToken) {
+    const redirectUrl = getPathToRedirect({
+      token,
+      urlOrigin: nextUrl.origin,
+      pathname: nextUrl.pathname,
+    })
+
+    if (redirectUrl !== undefined) {
+      return NextResponse.redirect(redirectUrl)
+    }
+
+    return NextResponse.next()
+  }
+}
+
+export const config = {
+  matcher: [
+    '/',
+    '/manutencao',
+    '/cadastrar',
+    '/verificar',
+    '/alterar-email',
+    '/jogar',
+    '/conta-inativa',
+    '/conectar',
+  ],
+}
