be0e7b371630e6cc3477138cf235bf714d0a00a6
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-10-25 10:51:30 -0300

    fix exports

---
+++ src/components/pages/connect/index.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+export * from './connectAvatarImage/ConnectAvatarImage';
+export * from './connectIpMessage/ConnectIpMessage';
+export * from './connectLogo/ConnectLogo';
+export * from './connectTimer/ConnectTimer';
+
