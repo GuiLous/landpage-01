1bb6270601301174f987a9031a916c87d568564b
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-10-30 18:42:44 -0300

    add new prop

--- src/components/shared/tooltip/Tooltip.tsx
+++ src/components/shared/tooltip/Tooltip.tsx
@@ -13,15 +13,17 @@ interface TooltipProps {
   side?: Position
   asChild?: boolean
   open?: boolean
+  className?: string
 }
 
 export function Tooltip({
   content,
   children,
-  delay = 300,
+  delay = 0,
   side = 'top',
   asChild = true,
   open,
+  className,
 }: TooltipProps) {
   return (
     <PrimitiveTooltip.Provider>
@@ -34,7 +36,7 @@ export function Tooltip({
           <PrimitiveTooltip.Content
             sideOffset={0}
             className={twMerge(
-              'z-50 inline-flex w-full items-center bg-gray-1000 px-2 py-1.5',
+              'z-50 inline-flex w-full rounded-sm items-center bg-gray-1000 px-2 py-1.5',
               'radix-side-bottom:animate-slide-up-fade',
               'radix-side-left:animate-slide-right-fade',
               'radix-side-right:animate-slide-left-fade',
@@ -44,7 +46,12 @@ export function Tooltip({
           >
             <PrimitiveTooltip.Arrow className="fill-gray-1000" />
 
-            <span className="block text-sm font-semibold leading-none text-white">
+            <span
+              className={twMerge(
+                'block text-sm font-semibold leading-none text-white',
+                className
+              )}
+            >
               {content}
             </span>
           </PrimitiveTooltip.Content>
