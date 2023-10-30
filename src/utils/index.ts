4824737458a81f957a16cac2c5d4416d1639314c
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-10-30 18:42:17 -0300

    create util humanizeStatus

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,7 @@ export * from './checkRoutes';
 export * from './fakeSignInEmails';
 export * from './format';
 export * from './getPathToRedirect';
+export * from './humanizeStatus';
 export * from './uuid4';
 export * from './validators';
 
