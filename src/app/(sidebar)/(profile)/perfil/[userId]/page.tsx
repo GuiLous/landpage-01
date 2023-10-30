f9dbad3676063a74112b356ff492e788901264f1
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-10-30 18:45:11 -0300

    create Profile page

---
+++ src/app/(sidebar)/(profile)/perfil/[userId]/page.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { useEffect } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+import { useAppSelector } from '@/store'
+
+import {
+  Loading,
+  ProfileHeader,
+  ProfileHeaderTabsButtons,
+} from '@/components/shared'
+
+import { useProfileDetails } from '@/hooks'
+
+interface ProfileProps {
+  params: { userId: string }
+}
+
+export default function Profile({ params }: ProfileProps) {
+  const { user } = useAppSelector((state) => state.user)
+  const { userId } = params
+
+  const { fetching, profile, getProfileDetails } = useProfileDetails()
+
+  const isUserLogged = Number(userId) === user?.id
+
+  useEffect(() => {
+    getProfileDetails(Number(userId))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userId])
+
+  return fetching ? (
+    <Loading.Overlay>
+      <Loading.Gif />
+    </Loading.Overlay>
+  ) : (
+    <main className={twMerge('flex-col gap-10', '3xl:gap-[1.875rem]')}>
+      <ProfileHeader isUserLogged={isUserLogged} profile={profile}>
+        <ProfileHeaderTabsButtons
+          isUserLogged={isUserLogged}
+          userId={Number(userId)}
+          username={profile.username}
+        />
+      </ProfileHeader>
+
+      <section>
+        <div>
+          <div>Stats</div>
+          <div>HeatMap</div>
+        </div>
+
+        <div>MatchHistory</div>
+      </section>
+    </main>
+  )
+}
