8a35862001a7bde11c8939bf26460193786ba7ef
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-10-31 18:07:39 -0300

    render ProfileLevelStatsCard

--- src/app/(sidebar)/(profile)/perfil/[userId]/page.tsx
+++ src/app/(sidebar)/(profile)/perfil/[userId]/page.tsx
@@ -7,6 +7,8 @@ import { useProfileDetails } from '@/contexts'
 
 import { useAppSelector } from '@/store'
 
+import { ProfileLevelStatsCard } from '@/components/pages'
+
 import {
   Loading,
   ProfileHeader,
@@ -44,14 +46,33 @@ export default function Profile({ params }: ProfileProps) {
         />
       </ProfileHeader>
 
-      <section>
-        <div>
-          <div>Stats</div>
+      <aside
+        className={twMerge(
+          'flex-initial gap-4 px-[3.750rem]',
+          '3xl:px-[1.875rem]'
+        )}
+      >
+        <div
+          className={twMerge(
+            'flex-col gap-4 self-start max-w-[350px]',
+            '3xl:max-w-[300px]'
+          )}
+        >
+          <ProfileLevelStatsCard
+            level={profile.level}
+            highest_level={profile.highest_level}
+            matches_won={profile.matches_won}
+            highest_win_streak={profile.highest_win_streak}
+            latest_matches_results={profile.latest_matches_results}
+            most_kills_in_a_match={profile.most_kills_in_a_match}
+            most_damage_in_a_match={profile.most_damage_in_a_match}
+            stats={profile.stats}
+          />
           <div>HeatMap</div>
         </div>
 
         <div>MatchHistory</div>
-      </section>
+      </aside>
     </main>
   )
 }
