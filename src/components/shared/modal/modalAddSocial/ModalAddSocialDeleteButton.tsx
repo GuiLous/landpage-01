619edf93e4b9e6814cc48cfa782f919d4a567782
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-10-31 13:30:40 -0300

    create ModalAddSocialDeleteButton component

---
+++ src/components/shared/modal/modalAddSocial/ModalAddSocialDeleteButton.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { RiCloseFill } from 'react-icons/ri'
+import { twMerge } from 'tailwind-merge'
+
+import { SocialHandles } from '@/contexts'
+
+import { CustomIcon, SocialHashType } from '@/components/shared'
+
+interface ModalAddSocialDeleteButtonProps {
+  socials: SocialHandles
+  item: SocialHashType
+  handleDeleteSocials: (item: SocialHashType) => void
+}
+
+export function ModalAddSocialDeleteButton({
+  handleDeleteSocials,
+  item,
+  socials,
+}: ModalAddSocialDeleteButtonProps) {
+  return (
+    <div className="max-w-fit flex-initial items-center gap-3">
+      <span className="text-sm font-medium leading-none text-white">
+        {socials[item]}
+      </span>
+
+      <CustomIcon
+        icon={RiCloseFill}
+        size={18}
+        className={twMerge(
+          'cursor-pointer transition-colors',
+          'hover:text-gray-300'
+        )}
+        onClick={() => handleDeleteSocials(item)}
+      />
+    </div>
+  )
+}
