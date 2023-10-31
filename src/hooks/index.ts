1fb0dde0b2e242679f1106866f37c416eccc1b20
Guilherme <guilhermesilva@acad.ifma.edu.br>
2023-10-31 13:35:02 -0300

    fix exports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,6 +4,5 @@ export * from './useAuth';
 export * from './useInitializeSlices';
 export * from './useLatestMatchesResults';
 export * from './usePersistentTimer';
-export * from './useProfileDetails';
 export * from './useShowErrorToast';
 
