c73f98de2cb9ec496e2c3ac405a50cd859f13b17
GuiLou <guilhermesilva@acad.ifma.edu.br>
2024-01-10 10:11:36 -0300

    add new methods

--- src/modelsApi/storeApi.ts
+++ src/modelsApi/storeApi.ts
@@ -1,6 +1,45 @@
 import { baseApi } from './baseApi'
 
 export const storeApi = {
+  async listStore(token: string, options?: RequestInit) {
+    return await baseApi.list({ endpoint: '/store/', token, options })
+  },
+  async listProducts(token: string, options?: RequestInit) {
+    return await baseApi.list({ endpoint: '/store/products/', token, options })
+  },
+  async buyProduct(token: string, payload: any, options?: RequestInit) {
+    return await baseApi.create({
+      endpoint: '/store/products/',
+      token,
+      payload,
+      options,
+    })
+  },
+  async buyItem(token: string, item_id: number, options?: RequestInit) {
+    return await baseApi.create({
+      endpoint: `/store/items/${item_id}`,
+      token,
+      options,
+    })
+  },
+  async buyBox(token: string, box_id: number, options?: RequestInit) {
+    return await baseApi.create({
+      endpoint: `/store/boxes/${box_id}`,
+      token,
+      options,
+    })
+  },
+  async buyCollection(
+    token: string,
+    collection_id: number,
+    options?: RequestInit
+  ) {
+    return await baseApi.create({
+      endpoint: `/store/collections/${collection_id}`,
+      token,
+      options,
+    })
+  },
   async listInventory(token: string, options?: RequestInit) {
     return await baseApi.list({ endpoint: '/store/inventory/', token, options })
   },
