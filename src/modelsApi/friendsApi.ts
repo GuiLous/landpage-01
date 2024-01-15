0916cd81b7a3650dab7c225f1cefb4fb6fd89ac0
GuiLou <guilhermesilva@acad.ifma.edu.br>
2024-01-15 15:51:37 -0300

    add new methods to friend model api

--- src/modelsApi/friendsApi.ts
+++ src/modelsApi/friendsApi.ts
@@ -4,4 +4,36 @@ export const friendsApi = {
   async list(token: string, options?: RequestInit) {
     return await baseApi.list({ endpoint: 'friends/', token, options })
   },
+
+  async add(token: string, username: string, options?: RequestInit) {
+    return await baseApi.create({
+      endpoint: `friends/${username}/`,
+      token,
+      options,
+    })
+  },
+
+  async remove(token: string, user_id: number, options?: RequestInit) {
+    return await baseApi.delete({
+      endpoint: `friends/${user_id}/`,
+      token,
+      options,
+    })
+  },
+
+  async accept(token: string, request_id: number, options?: RequestInit) {
+    return await baseApi.create({
+      endpoint: `friends/requests/${request_id}/`,
+      token,
+      options,
+    })
+  },
+
+  async refuse(token: string, request_id: number, options?: RequestInit) {
+    return await baseApi.delete({
+      endpoint: `friends/requests/${request_id}/`,
+      token,
+      options,
+    })
+  },
 }
