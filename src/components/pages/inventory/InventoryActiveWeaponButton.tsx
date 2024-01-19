4e57af30c2d3150bd4a06627b40a01c1ebf84ea2
GuiLou <guilhermesilva@acad.ifma.edu.br>
2024-01-19 17:56:47 -0300

    create InventoryActiveWeaponButton component

---
+++ src/components/pages/inventory/InventoryActiveWeaponButton.tsx
@@ -0,0 +1,53 @@
+import { useCallback } from 'react'
+
+import { StoreItem } from '@/functions'
+
+import { Button } from '@/components/shared'
+
+interface InventoryActiveWeaponButtonProps {
+  weapon_id?: number
+  in_use?: boolean
+  hasItemInUse?: boolean
+  itemInUse?: StoreItem
+  handleUpdateItemInUse: ({
+    item_id,
+    updateSelected,
+  }: {
+    item_id: number
+    updateSelected?: boolean
+  }) => void
+}
+
+export function InventoryActiveWeaponButton({
+  weapon_id,
+  handleUpdateItemInUse,
+  in_use = false,
+  hasItemInUse = false,
+  itemInUse,
+}: InventoryActiveWeaponButtonProps) {
+  const isNullItemSelected = weapon_id === 0 && hasItemInUse
+
+  const updateItem = useCallback(() => {
+    if (weapon_id === 0 && itemInUse?.id)
+      return handleUpdateItemInUse({
+        item_id: itemInUse.id,
+        updateSelected: false,
+      })
+
+    if (weapon_id) {
+      handleUpdateItemInUse({ item_id: weapon_id })
+    }
+  }, [handleUpdateItemInUse, itemInUse?.id, weapon_id])
+
+  return (
+    <Button.Root
+      className="max-h-[42px] min-h-[42px] w-[204px]"
+      onClick={updateItem}
+      restricted={in_use || isNullItemSelected}
+    >
+      <Button.Content className="text-sm font-semibold">
+        {in_use || isNullItemSelected ? 'Remover skin' : 'Equipar skin'}
+      </Button.Content>
+    </Button.Root>
+  )
+}
