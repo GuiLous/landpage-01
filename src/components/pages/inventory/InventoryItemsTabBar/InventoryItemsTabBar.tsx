4fb1f064f8afb57325aae0724618205c87c4d196
GuiLou <guilhermesilva@acad.ifma.edu.br>
2024-01-19 17:57:31 -0300

    remove comments

--- src/components/pages/inventory/InventoryItemsTabBar/InventoryItemsTabBar.tsx
+++ src/components/pages/inventory/InventoryItemsTabBar/InventoryItemsTabBar.tsx
@@ -1,11 +1,5 @@
-// import { twMerge } from 'tailwind-merge'
-
-// import { Switch } from '@/components/shared'
-
 import { SubTabTypes, TabTypes } from '../InventoryWrapperContent'
-// import { InventoryItemsTabBarCt } from './InventoryItemsTabBarCt'
 import { InventoryItemsTabBarTabs } from './InventoryItemsTabBarTabs'
-// import { InventoryItemsTabBarTr } from './InventoryItemsTabBarTr'
 
 interface InventoryItemsTabBarProps {
   activeTab: string
@@ -31,33 +25,6 @@ export function InventoryItemsTabBar({
         setActiveSubTab={setActiveSubTab}
         setActiveTab={setActiveTab}
       />
-
-      {/* <div
-        className={twMerge(
-          'max-w-fit flex-initial items-center gap-2.5',
-          disableSideSelect && 'opacity-30'
-        )}
-      >
-        <InventoryItemsTabBarTr
-          disableSideSelect={disableSideSelect}
-          isChecked={isChecked}
-          setIsChecked={setIsChecked}
-        />
-
-        <Switch
-          onCheckedChange={() => setIsChecked(!isChecked)}
-          checked={disableSideSelect ? false : isChecked}
-          disabled={disableSideSelect}
-        >
-          <Switch.Thumb />
-        </Switch>
-
-        <InventoryItemsTabBarCt
-          disableSideSelect={disableSideSelect}
-          isChecked={isChecked}
-          setIsChecked={setIsChecked}
-        />
-      </div> */}
     </div>
   )
 }
