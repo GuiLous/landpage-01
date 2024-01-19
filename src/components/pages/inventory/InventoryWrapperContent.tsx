50525b881637234675169e6d1528c0a4c75b6c40
GuiLou <guilhermesilva@acad.ifma.edu.br>
2024-01-19 18:01:49 -0300

    create arsenal on inventory

--- src/components/pages/inventory/InventoryWrapperContent.tsx
+++ src/components/pages/inventory/InventoryWrapperContent.tsx
@@ -5,22 +5,26 @@ import { twMerge } from 'tailwind-merge'
 
 import { TABS_NO_SWITCH } from '@/constants'
 
-import { revalidate } from '@/utils'
+import { WeaponNameType, WeaponType, revalidate } from '@/utils'
 
 import { Inventory, StoreItem } from '@/functions'
 
 import { storeApi } from '@/modelsApi'
 
 import {
+  CenteredCarouselWrapper,
   CustomScrollBar,
   ImagePreview,
   ItemsSelectList,
+  WeaponVideoPreview,
+  WeaponsSelectList,
 } from '@/components/shared'
 
 import { useAuth, useShowErrorToast } from '@/hooks'
 
 import notSelected from '@/assets/images/not_selected.png'
 
+import { InventoryActiveWeaponButton } from './InventoryActiveWeaponButton'
 import { InventoryItemDescription } from './InventoryItemDescription'
 import { InventoryItemsTabBar } from './InventoryItemsTabBar/InventoryItemsTabBar'
 import { InventorySubItemTab } from './InventorySubItemTab'
@@ -75,8 +79,8 @@ const sub_tabs = {
   avatar: 'persona',
   roupas: 'wear',
   sprays: 'spray',
-  pistolas: 'pistol',
-  submetralhadoras: 'submachineguns',
+  pistolas: 'pistols',
+  submetralhadoras: 'smgs',
   escopetas: 'shotguns',
   metralhadoras: 'machineguns',
   fuzis: 'rifles',
@@ -90,6 +94,12 @@ interface InventoryWrapperContentProps {
   inventory: Inventory
 }
 
+const nullObject = {
+  id: 0,
+  foreground_image: notSelected.src,
+  in_use: false,
+}
+
 export function InventoryWrapperContent({
   inventory,
 }: InventoryWrapperContentProps) {
@@ -99,22 +109,21 @@ export function InventoryWrapperContent({
 
   const [activeTab, setActiveTab] = useState<TabTypes>('arsenal')
   const [activeSubTab, setActiveSubTab] = useState<SubTabTypes>('pistolas')
-  const [itemsByType, setItemsByType] = useState<Partial<Item[]>>([
-    {
-      id: 0,
-      foreground_image: notSelected.src,
-      in_use: false,
-    },
-  ])
+  const [itemsByType, setItemsByType] = useState<Partial<Item[]>>([])
   const [itemSelected, setItemSelected] = useState<StoreItem | null>(null)
   const [newItemSelected, setNewItemSelected] = useState<StoreItem | null>(null)
-  const [activeItemType, setActiveItemType] = useState<SubTabTypes>('avatar')
+  const [activeItemType, setActiveItemType] = useState<SubTabTypes>('pistolas')
   const [isChecked, setIsChecked] = useState(false)
+  const [weaponSelected, setWeaponSelected] =
+    useState<WeaponNameType>('Pistola')
+  const [activeItemIndex, setActiveItemIndex] = useState(0)
 
   const disableSwitch = TABS_NO_SWITCH.includes(activeItemType)
-
   const itemInUse = itemsByType.find((item) => item?.in_use)
   const hasItemInUse = !!itemInUse
+
+  const isNullWeapon = itemsByType[activeItemIndex]?.id === 0
+  const isArsenal = activeTab === 'arsenal'
 
   const filterItemsByType = useCallback(() => {
     if (activeTab === 'sprays') {
@@ -122,7 +131,7 @@ export function InventoryWrapperContent({
         (item) => item.item_type === sub_tabs[activeTab]
       )
 
-      setItemsByType((prevState) => [{ ...prevState[0] }, ...itemsFiltered])
+      setItemsByType([nullObject, ...itemsFiltered])
       return
     }
 
@@ -131,7 +140,24 @@ export function InventoryWrapperContent({
         (item) => item.subtype === sub_tabs[activeSubTab]
       )
 
-      setItemsByType((prevState) => [{ ...prevState[0] }, ...itemsFiltered])
+      setItemsByType([nullObject, ...itemsFiltered])
+      return
+    }
+
+    if (isArsenal) {
+      const itemsFilteredByType = inventory.items.filter(
+        (item) => item.item_type === 'weapon'
+      )
+
+      const itemsFilteredByWeapon = itemsFilteredByType.filter(
+        (item) => item.subtype === sub_tabs[activeSubTab]
+      )
+
+      const itemsFilteredByWeaponName = itemsFilteredByWeapon.filter(
+        (item) => item.weapon === weaponSelected
+      )
+
+      setItemsByType([nullObject, ...itemsFilteredByWeaponName])
       return
     }
 
@@ -139,16 +165,8 @@ export function InventoryWrapperContent({
       (item) => item.item_type === sub_tabs[activeSubTab]
     )
 
-    // if (!disableSwitch) {
-    //   if (isChecked) {
-    //     itemsFiltered = itemsFiltered.filter((item) => item.subtype === 'def')
-    //   } else {
-    //     itemsFiltered = itemsFiltered.filter((item) => item.subtype !== 'def')
-    //   }
-    // }
-
-    setItemsByType((prevState) => [{ ...prevState[0] }, ...itemsFiltered])
-  }, [activeTab, activeSubTab, inventory])
+    setItemsByType([nullObject, ...itemsFiltered])
+  }, [activeTab, activeSubTab, inventory, weaponSelected, isArsenal])
 
   const handleUpdateItemInUse = useCallback(
     async ({
@@ -193,7 +211,7 @@ export function InventoryWrapperContent({
     if (inventory?.items.length > 0) {
       filterItemsByType()
     }
-  }, [inventory, activeTab, activeSubTab, filterItemsByType])
+  }, [inventory, activeTab, activeSubTab, filterItemsByType, weaponSelected])
 
   useEffect(() => {
     if (itemsByType.length > 0) {
@@ -217,7 +235,13 @@ export function InventoryWrapperContent({
 
   useEffect(() => {
     setNewItemSelected(null)
-  }, [activeTab, activeSubTab])
+  }, [activeTab, activeSubTab, weaponSelected, activeItemIndex])
+
+  useEffect(() => {
+    if (isArsenal) {
+      setItemSelected(itemsByType[activeItemIndex] as StoreItem)
+    }
+  }, [activeItemIndex, itemsByType, weaponSelected, isArsenal])
 
   return (
     <>
@@ -244,19 +268,32 @@ export function InventoryWrapperContent({
                 activeSubTab={activeSubTab}
                 setActiveItemType={setActiveItemType}
                 setActiveSubTab={setActiveSubTab}
-                subTabs={tabs[activeTab].subTabs}
+                subTabs={tabs[activeTab].subTabs as SubTabTypes[]}
+                setWeaponSelected={setWeaponSelected}
               />
             )}
 
-            <CustomScrollBar height={312} className="max-w-fit">
-              <ItemsSelectList
-                hasItemInUse={hasItemInUse}
-                itemSelectedId={itemSelected?.id}
-                items={itemsByType as StoreItem[]}
-                setItemSelected={setItemSelected}
-                setNewItemSelected={setNewItemSelected}
-              />
-            </CustomScrollBar>
+            {isArsenal && (
+              <CustomScrollBar className="max-h-[375px] max-w-fit">
+                <WeaponsSelectList
+                  weaponSelected={weaponSelected}
+                  setWeaponSelected={setWeaponSelected}
+                  activeSubTab={activeSubTab as WeaponType}
+                />
+              </CustomScrollBar>
+            )}
+
+            {!isArsenal && (
+              <CustomScrollBar className="max-h-[312px] max-w-fit">
+                <ItemsSelectList
+                  hasItemInUse={hasItemInUse}
+                  itemSelectedId={itemSelected?.id}
+                  items={itemsByType as StoreItem[]}
+                  setItemSelected={setItemSelected}
+                  setNewItemSelected={setNewItemSelected}
+                />
+              </CustomScrollBar>
+            )}
           </div>
 
           {itemSelected && (
@@ -265,11 +302,47 @@ export function InventoryWrapperContent({
               item={itemSelected}
               itemType={activeItemType}
               itemInUse={itemInUse as StoreItem}
+              isArsenal={isArsenal}
             />
           )}
         </aside>
 
-        <ImagePreview itemSelected={itemSelected} />
+        <aside className={twMerge('flex-col gap-10', '3xl:gap-8')}>
+          <ImagePreview
+            itemSelected={itemSelected}
+            isArsenal={isArsenal}
+            hasSkins={itemsByType.length > 0}
+          />
+
+          {isArsenal && itemSelected && (
+            <section
+              className={twMerge('flex-initial flex-col gap-7', '3xl:gap-6')}
+            >
+              {((isNullWeapon && hasItemInUse) || !isNullWeapon) && (
+                <div className="min-h-[44px] items-center justify-between">
+                  <WeaponVideoPreview
+                    imagesPreview={itemsByType[activeItemIndex]?.media}
+                  />
+
+                  <InventoryActiveWeaponButton
+                    handleUpdateItemInUse={handleUpdateItemInUse}
+                    weapon_id={itemsByType[activeItemIndex]?.id}
+                    in_use={itemsByType[activeItemIndex]?.in_use}
+                    hasItemInUse={hasItemInUse}
+                    itemInUse={itemInUse as StoreItem}
+                  />
+                </div>
+              )}
+
+              <CenteredCarouselWrapper
+                data={itemsByType as StoreItem[]}
+                setActiveItemIndex={setActiveItemIndex}
+                hasItemInUse={hasItemInUse}
+                isInventory
+              />
+            </section>
+          )}
+        </aside>
       </div>
     </>
   )
