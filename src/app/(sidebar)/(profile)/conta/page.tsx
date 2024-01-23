09d9cafd0b777e26100d734a801bf29506af4ee1
GuiLou <guilhermesilva@acad.ifma.edu.br>
2024-01-23 19:21:49 -0300

    change config page to and decoratives

--- src/app/(sidebar)/(profile)/conta/page.tsx
+++ src/app/(sidebar)/(profile)/conta/page.tsx
@@ -2,13 +2,14 @@ import { twMerge } from 'tailwind-merge'
 
 import { getAuthServer } from '@/utils'
 
-import { getUserProfile } from '@/functions'
+import { getUserInventory, getUserProfile } from '@/functions'
 
 import {
   AccountChangeEmailCard,
   AccountDeleteCard,
   AccountInactiveCard,
-  AccountTitle,
+  AccountPlayerDecoration,
+  AccountSectionTitle,
 } from '@/components/pages'
 
 import { ProfileHeader, ProfileHeaderTabsButtons } from '@/components/shared'
@@ -17,6 +18,10 @@ export default async function Account() {
   const auth = getAuthServer()
 
   const profile = await getUserProfile(Number(auth.id))
+  const inventory = await getUserInventory()
+
+  const cards = inventory.items.filter((item) => item.subtype === 'card')
+  const covers = inventory.items.filter((item) => item.subtype === 'profile')
 
   return (
     <main className={twMerge('flex-col gap-10 pb-10', '3xl:gap-7 3xl:pb-7')}>
@@ -28,19 +33,41 @@ export default async function Account() {
         />
       </ProfileHeader>
 
-      <AccountTitle />
+      <section className={twMerge('flex-col gap-10', '3xl:gap-7')}>
+        <AccountSectionTitle title="personalização de perfil" />
 
-      <section
-        className={twMerge(
-          'flex-initial flex-col gap-6 px-[3.750rem]',
-          '3xl:px-7 3xl:gap-5'
-        )}
-      >
-        <AccountChangeEmailCard />
+        <div
+          className={twMerge(
+            'flex-initial flex-col gap-6 px-[3.750rem]',
+            '3xl:px-7 3xl:gap-5'
+          )}
+        >
+          <div className={twMerge('gap-10', '3xl:gap-7')}>
+            <AccountPlayerDecoration title="Card de jogador" items={cards} />
+            <AccountPlayerDecoration
+              title="Capa de perfil"
+              items={covers}
+              isProfileCover
+            />
+          </div>
+        </div>
+      </section>
 
-        <div className={twMerge('gap-6', '3xl:gap-5')}>
-          <AccountInactiveCard />
-          <AccountDeleteCard />
+      <section className={twMerge('flex-col gap-10', '3xl:gap-7')}>
+        <AccountSectionTitle title="configurações de conta" />
+
+        <div
+          className={twMerge(
+            'flex-initial flex-col gap-6 px-[3.750rem]',
+            '3xl:px-7 3xl:gap-5'
+          )}
+        >
+          <AccountChangeEmailCard />
+
+          <div className={twMerge('gap-6', '3xl:gap-5')}>
+            <AccountInactiveCard />
+            <AccountDeleteCard />
+          </div>
         </div>
       </section>
     </main>
