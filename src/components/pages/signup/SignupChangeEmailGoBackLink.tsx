43238ee189586e16fedb33a3c3696f0ff7e9ba75
GuiLou <guilhermesilva@acad.ifma.edu.br>
2024-01-29 14:37:52 -0300

    align text on end

--- src/components/pages/signup/SignupChangeEmailGoBackLink.tsx
+++ src/components/pages/signup/SignupChangeEmailGoBackLink.tsx
@@ -2,7 +2,7 @@ import { Link } from '@/components/shared'
 
 export function SignupChangeEmailGoBackLink() {
   return (
-    <div className="items-center justify-center">
+    <div className="items-end justify-center">
       <Link href="/verificar">Cancelar e voltar</Link>
     </div>
   )
