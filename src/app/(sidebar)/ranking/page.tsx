f263791b2b6136572df6928499e32f5d178c488e
GuiLou <guilhermesilva@acad.ifma.edu.br>
2024-02-08 23:16:47 -0300

    create Ranking page

---
+++ src/app/(sidebar)/ranking/page.tsx
@@ -0,0 +1,82 @@
+import { twMerge } from 'tailwind-merge'
+
+import { getRanking } from '@/functions'
+
+import { RankingHeader, RankingTopPlayersCard } from '@/components/pages'
+
+import { Pagination, RankingTable } from '@/components/shared'
+import { FieldsTableType } from '@/components/shared/rankingTable/RankingTableHeader'
+
+const fieldsTable = [
+  {
+    stat: 'Posição',
+    label: 'Posição no top 100',
+  },
+  {
+    stat: 'Nome de usuário',
+    label: 'Nome do jogador',
+  },
+  {
+    stat: 'Rank',
+    label: 'Nível na reload',
+  },
+  {
+    stat: 'Level',
+    label: 'Nível na reload',
+  },
+  {
+    stat: 'Win Rate',
+    label: 'Taxa de vitórias',
+  },
+  {
+    stat: 'KDA',
+    label: 'Abates, Mortes, Assistências',
+  },
+]
+
+interface RouteProps {
+  searchParams: { page: string }
+}
+
+export default async function Ranking({ searchParams }: RouteProps) {
+  const { page } = searchParams
+
+  const ranking = await getRanking({ page: page ? Number(page) : 1 })
+  const topPlayers = ranking.list.splice(0, 3)
+
+  return (
+    <main
+      className={twMerge(
+        'flex-col gap-10 py-10 px-[4%]',
+        '3xl:gap-8',
+        'ultrawide:gap-14'
+      )}
+    >
+      <RankingHeader />
+
+      <section className={twMerge('flex-initial gap-6', '3xl:gap-4')}>
+        {topPlayers.map((player) => (
+          <RankingTopPlayersCard key={player.user_id} player={player} />
+        ))}
+      </section>
+
+      <RankingTable.Root>
+        <RankingTable.Header fieldsTable={fieldsTable as FieldsTableType[]} />
+
+        <RankingTable.Body
+          players={ranking.list}
+          fieldsColumn={fieldsTable as FieldsTableType[]}
+        />
+      </RankingTable.Root>
+
+      {ranking?.total_pages > 1 && (
+        <div className={twMerge('flex-initial items-end justify-center pb-10')}>
+          <Pagination
+            totalPages={ranking?.total_pages}
+            currentPage={ranking?.current_page}
+          />
+        </div>
+      )}
+    </main>
+  )
+}
