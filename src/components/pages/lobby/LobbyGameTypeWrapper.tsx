11326f614ea7a0942ab242809fbf8be238d80be0
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2024-02-22 11:48:00 -0300

    update lobby on switch game type

--- src/components/pages/lobby/LobbyGameTypeWrapper.tsx
+++ src/components/pages/lobby/LobbyGameTypeWrapper.tsx
@@ -1,16 +1,57 @@
 'use client'
 
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
-import { Lineup, LineupCustom } from '@/components/shared'
+import { useLobbyStore } from '@/store/lobbyStore'
+import { GameType } from '@/store/matchStore'
+
+import { lobbyApi } from '@/modelsApi'
+
+import { Lineup, LineupCustom, SkeletonLineupCustom } from '@/components/shared'
+
+import { useAuth } from '@/hooks'
 
 import { LobbyGameType } from './LobbyGameType'
 import { LobbyHeader } from './LobbyHeader'
 
 export type LobbyGameType = 'TDM 5X5' | 'RANQUEADA 5X5' | 'PERSONALIZADA'
 
+const gameTypeConvert = {
+  competitive: 'RANQUEADA 5X5',
+  custom: 'PERSONALIZADA',
+}
 export function LobbyGameTypeWrapper() {
-  const [activeTab, setActiveTab] = useState<LobbyGameType>('RANQUEADA 5X5')
+  const { lobby } = useLobbyStore()
+
+  const auth = useAuth()
+
+  const [activeTab, setActiveTab] = useState<LobbyGameType>(
+    lobby?.mode
+      ? (gameTypeConvert[lobby.mode] as LobbyGameType)
+      : 'RANQUEADA 5X5'
+  )
+  const [isFetching, setIsFetching] = useState(true)
+
+  const updateLobbyType = useCallback(
+    async (mode: GameType) => {
+      if (!auth?.token || !lobby?.id) return
+
+      await lobbyApi.switchMode(auth.token, lobby.id, mode)
+
+      if (activeTab === 'PERSONALIZADA') setIsFetching(false)
+    },
+    [auth?.token, lobby?.id, activeTab]
+  )
+
+  useEffect(() => {
+    updateLobbyType(activeTab === 'RANQUEADA 5X5' ? 'competitive' : 'custom')
+  }, [activeTab, updateLobbyType])
+
+  useEffect(() => {
+    if (lobby?.mode) {
+      setActiveTab(gameTypeConvert[lobby.mode] as LobbyGameType)
+    }
+  }, [lobby?.mode])
 
   return (
     <>
@@ -19,7 +60,8 @@ export function LobbyGameTypeWrapper() {
       <LobbyGameType activeTab={activeTab} setActiveTab={setActiveTab} />
 
       {activeTab === 'RANQUEADA 5X5' && <Lineup />}
-      {activeTab === 'PERSONALIZADA' && <LineupCustom />}
+      {activeTab === 'PERSONALIZADA' && isFetching && <SkeletonLineupCustom />}
+      {activeTab === 'PERSONALIZADA' && !isFetching && <LineupCustom />}
     </>
   )
 }
