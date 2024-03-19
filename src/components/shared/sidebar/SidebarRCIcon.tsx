1e6ed236a7d5b14b790b762e584806c49ef7350a
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2024-03-19 13:38:03 -0300

    disable lobby option based on feats array

--- src/components/shared/sidebar/SidebarRCIcon.tsx
+++ src/components/shared/sidebar/SidebarRCIcon.tsx
@@ -6,16 +6,19 @@ import { Button } from '@/components/shared'
 export function SidebarRCIcon() {
   return (
     <Button.Root
+      asChild
       className={twMerge(
         'max-h-9 min-h-9 min-w-9 rounded-e-none',
         '3xl:max-h-8 3xl:min-h-8 3xl:min-w-8',
         'ultrawide:max-h-14 ultrawide:min-h-14 ultrawide:min-w-14'
       )}
     >
-      <Button.Icon
-        icon={BiPlus}
-        className={twMerge('text-2xl', 'ultrawide:text-3xl')}
-      />
+      <div className="max-w-fit flex-initial">
+        <Button.Icon
+          icon={BiPlus}
+          className={twMerge('text-2xl', 'ultrawide:text-3xl')}
+        />
+      </div>
     </Button.Root>
   )
 }
