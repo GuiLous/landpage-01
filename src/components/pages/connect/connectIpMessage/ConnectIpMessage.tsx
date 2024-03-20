6cbf82afb7b89c25d06d2ea70a062506991be051
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2024-03-20 11:39:39 -0300

    add quotes on text

--- src/components/pages/connect/connectIpMessage/ConnectIpMessage.tsx
+++ src/components/pages/connect/connectIpMessage/ConnectIpMessage.tsx
@@ -22,8 +22,9 @@ export function ConnectIpMessage() {
           'ultrawide:text-3xl ultrawide:max-w-[895px]'
         )}
       >
-        Para jogar, clique em Conectar FiveM. Ou se preferir, insira o IP abaixo
-        na lista de servidores.{' '}
+        Para jogar, clique em &ldquo;
+        <b className="font-bold">Conectar FiveM</b>&ldquo;. Ou se preferir,
+        insira o IP abaixo na lista de servidores.{' '}
         <span className="block font-medium text-cyan-400">
           Você tem até 5 minutos para se conectar.
         </span>
